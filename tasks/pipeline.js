a17a5c62310d1ff26d94dd1a23c00005524f3db7
Akseli Palen <akseli.palen@gmail.com>
2016-09-03 00:26:56 +0300

    Allow .ejs client-side templates instead .html

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -47,7 +47,7 @@ var jsFilesToInject = [
 // templates get spit out to the same file.  Be sure and check out `tasks/README.md`
 // for information on customizing and installing new tasks.
 var templateFilesToInject = [
-  'templates/**/*.html'
+  'templates/**/*.ejs'
 ];
 
 
@@ -83,5 +83,3 @@ module.exports.templateFilesToInject = templateFilesToInject.map(function(tplPat
   }
   return require('path').join('assets/',tplPath);
 });
-
-
