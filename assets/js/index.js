499ccae869834c8386db3dd890d78c216a0aa169
Akseli Palen <akseli.palen@gmail.com>
2016-09-03 00:28:05 +0300

    Search and add buttons with a marker and an initial rendered template.

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,12 +11,13 @@ function initMap() {
     zoom: 8
   });
 
-  /*
+
   (function initMapMenu() {
     // Main menu
     var menuDiv = document.createElement('div');
+    menuDiv.className = 'tresdb-map-menu';
 
-    var newsUI = document.createElement('img');
+    /*var newsUI = document.createElement('img');
     newsUI.src = 'images/icons/Messaging-Appointment-Reminders-icon.png';
     menuDiv.appendChild(newsUI);
 
@@ -30,18 +31,63 @@ function initMap() {
 
     var userUI = document.createElement('img');
     userUI.src = 'images/icons/Users-Name-icon.png';
-    menuDiv.appendChild(userUI);
+    menuDiv.appendChild(userUI);*/
+
+    (function defineAddButton() {
+
+      var addUI = document.createElement('div');
+      addUI.className = 'tresdb-map-menu-button-ui';
+
+      var addText = document.createElement('div');
+      addText.className = 'tresdb-map-menu-button-text';
+
+      addText.innerHTML = 'Add';
+      addUI.appendChild(addText);
+
+      addUI.addEventListener('click', function () {
+        var m = new google.maps.Marker({
+          position: map.getCenter(),
+          title: 'New location',
+          draggable: true,
+          animation: google.maps.Animation.DROP
+        });
+        m.setMap(map);
+      });
+
+      menuDiv.appendChild(addUI);
+    }());
+
+    (function defineSearchButton() {
+
+      var searchUI = document.createElement('div');
+      searchUI.className = 'tresdb-map-menu-button-ui';
+
+      var searchText = document.createElement('div');
+      searchText.className = 'tresdb-map-menu-button-text';
+
+      searchText.innerHTML = 'Search';
+      searchUI.appendChild(searchText);
+
+      searchUI.addEventListener('click', function () {
+        // Open a card over the map.
+        var cardContainer = document.getElementById('card-container');
+        cardContainer.style.display = 'block';  // from 'none'
+
+        // Render with a precompiled template provided by Sails.
+        var cardInner = window.JST['assets/templates/card.ejs']({
+          message: 'Hello'
+        });
+
+        var card = document.createElement('div');
+        card.innerHTML = cardInner;
+        cardContainer.appendChild(card);
+      });
+
+      menuDiv.appendChild(searchUI);
+
+    }());
 
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(menuDiv);
 
-    addUI.addEventListener('click', function () {
-      var m = new google.maps.Marker({
-        position: map.getCenter(),
-        title: 'New location',
-        draggable: true,
-        animation: google.maps.Animation.DROP
-      });
-      m.setMap(map);
-    });
-  }());*/
+  }());
 }
