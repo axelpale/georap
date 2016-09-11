2d813a54a532d289c3fca116aef6b2a5fdc8dc3c
Akseli Palen <akseli.palen@gmail.com>
2016-09-11 23:42:14 +0300

    Create index for unique name

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -24,19 +24,27 @@ module.exports = function (db) {
         console.error(err);
         return;
       }
+      users.ensureIndex({ name: 1 }, { unique: true }, function (err, result) {
+        if (err) {
+          // Documents in the collection makes this index impossible.
+          console.error('config/bootstrap: ensureIndex error');
+          console.error(err);
+          return;
+        }
 
-      users.insert(user)
-        .then(function (inserted) {
-          console.log('Default user inserted');
-        })
-        .catch(function (err) {
-          if (err.code === 11000) {
-            // User with this email already exists. Nice :)
-          } else {
-            console.error('config/bootstrap: insert error');
-            console.error(err);
-          }
-        });
+        users.insert(user)
+          .then(function (inserted) {
+            console.log('Default user inserted');
+          })
+          .catch(function (err) {
+            if (err.code === 11000) {
+              // User with this email already exists. Nice :)
+            } else {
+              console.error('config/bootstrap: insert error');
+              console.error(err);
+            }
+          });
+      });
     });
 
 
