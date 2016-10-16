0f79c9ac1ddb73a2a13001a4ae563d36993c196e
Akseli Palen <akseli.palen@gmail.com>
2016-10-17 00:52:36 +0300

    Implement schema model

--- migration/lib/schema.js
+++ migration/lib/schema.js
@@ -1,9 +1,33 @@
 // Tools to find out and update the current database schema version.
 
-exports.getSchemaVersion = function (db, callback) {
-  return callback(new Error('not implemented'));
+exports.getVersion = function (configColl, callback) {
+  // Find current schema version.
+  //
+  // Parameters:
+  //   configColl
+  //     A MongoDB collection where the schema version is kept.
+  //   callback
+  //     function (err, version)
+
+  configColl.findOne({key: 'schemaVersion'}).then(function (doc) {
+    if (doc) {
+      return callback(null, doc.value);
+    }  // else
+    // No schema found. Must be v1.
+    return callback(null, 1);
+  }).catch(function (err) {
+    return callback(err, null);
+  });
 };
 
-exports.setSchemaVersion = function (db, version, callback) {
-  return callback(new Error('not implemented'));
+exports.setVersion = function (configColl, version, callback) {
+  // Update database schema version.
+
+  configColl.update({ key: 'schemaVersion' }, {
+    $set: { value: version }
+  }).then(function () {
+    callback(null);
+  }).catch(function (err) {
+    callback(err);
+  });
 };
