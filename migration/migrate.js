7907bbbf92a1c729d9ac4c1754527e429a66d27a
Akseli Palen <akseli.palen@gmail.com>
2016-10-17 00:53:35 +0300

    migrate.js checks versions
    But does not run migration yet.

--- migration/migrate.js
+++ migration/migrate.js
@@ -1,2 +1,30 @@
 
-console.log('Migration successful.');
+var pjson = require('../package.json');
+var schema = require('./lib/schema');
+var local = require('../config/local');
+var monk = require('monk');
+var semver = require('semver');
+
+// DB
+var db = monk(local.mongo.url);
+
+// Desired version
+var targetVersion = semver.major(pjson.version);
+
+schema.getVersion(db.get('config'), function (err, currentVersion) {
+  if (err) {
+    throw err;
+  }  // else
+
+  console.log('Current DB schema version:', currentVersion);
+  console.log('Desired DB schema version:', targetVersion);
+
+  if (currentVersion < targetVersion) {
+    console.log('Migration from', currentVersion, 'to',
+                targetVersion, 'is needed.');
+  } else {
+    console.log('Migration successful.');
+  }
+
+  return db.close();
+});
