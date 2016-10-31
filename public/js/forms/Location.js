260c4551d8dd1f24a227b044c2e3b801907aae30
Akseli Palen <akseli.palen@gmail.com>
2016-10-31 20:37:09 +0200

    client: order content entries by time

--- public/js/forms/Location.js
+++ public/js/forms/Location.js
@@ -9,10 +9,25 @@ module.exports = function (loc) {
   //   loc
   //     Location object
 
+  // Init
+
+  // Sort content, newest first, create-event to bottom.
+  loc.content.sort(function comp(a, b) {
+    if (a.time < b.time) {
+      return 1;
+    }
+    if (a.time > b.time) {
+      return -1;
+    }
+    if (a.type === 'created') {
+      return 1;
+    }
+    return 0;
+  });
+
   // Private methods declaration
 
   // Public methods
-
 
   this.render = function () {
 
