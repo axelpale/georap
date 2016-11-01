f230579506561dd2b6f256b4b634cb76588809e4
Akseli Palen <akseli.palen@gmail.com>
2016-11-01 19:26:46 +0200

    server: remove deprecated get all handler

--- server/handlers/locations.js
+++ server/handlers/locations.js
@@ -63,42 +63,6 @@ exports.getOne = function (db, data, response) {
   });
 };
 
-exports.get = function (db, data, response) {
-  // Parameters
-  //   db
-  //     Monk db instance
-  //   data
-  //     JWT token and query filters
-  //   response
-  //     Socket.io response
-
-  throw new Error('deprecated');
-
-  jwt.verify(data.token, local.secret, function (err) {
-    if (err) {
-      // Problems with token
-
-      return response(errors.responses.InvalidTokenError);
-    }  // else
-
-    // Give all locations. TODO take data and payload into account which
-    // locations to fetch.
-    var locations = db.get('locations');
-
-    locations.find({}).then(function (locs) {
-      return response({
-        locations: locs,
-      });
-    }).catch(function (err2) {
-      console.error(err2);
-
-      return response({
-        error: 'db-query-error',
-      });
-    });
-  });
-};
-
 exports.getWithin = function (db, data, response) {
   // Parameters:
   //   db
