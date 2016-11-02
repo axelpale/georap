c7ca7649f23da96140390bb5411f126fb5cfb3f5
Akseli Palen <akseli.palen@gmail.com>
2016-11-02 22:19:56 +0200

    migration: migrate takes db and optional targetVersion

--- migration/migrate.js
+++ migration/migrate.js
@@ -1,7 +1,17 @@
+var local = require('../config/local');
 var migrates = require('./lib/migrates');
+var monk = require('monk');
 
-migrates.migrate(function (err) {
-  if (err) {
-    console.error(err);
-  }
+// DB
+var db = monk(local.mongo.url);
+
+migrates.migrate({
+  db: db,
+  callback: function (err) {
+    if (err) {
+      console.error(err);
+    }
+
+    db.close();
+  },
 });
