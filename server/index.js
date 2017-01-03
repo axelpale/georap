96170ee26ddf7103b5c3966026d8ca75a461ddc1
Akseli Palén <akseli.palen@gmail.com>
2017-01-03 21:20:38 +0200

    server: refactor index, remove monk

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-statements */
 
 var local = require('../config/local');
 var http = require('http');
@@ -8,7 +9,7 @@ var server = http.createServer(app);
 var io = require('socket.io')(server);
 
 var nodemailer = require('nodemailer');
-var monk = require('monk');
+var mongoClient = require('mongodb').MongoClient;
 
 var path = require('path');
 
@@ -28,157 +29,160 @@ var routes = require('./routes');
 console.log('Starting TresDB in environment:', local.env);
 
 
-// Database
-var db;
+// Database connection first
 
+var mongoUrl;
 if (local.env === 'test') {
-  db = monk(local.mongo.testUrl);
+  mongoUrl = local.mongo.testUrl;
 } else {
-  db = monk(local.mongo.url);
+  mongoUrl = local.mongo.url;
 }
 
-db.then(function () {
-  console.log('Connected to MongoDB...');
-}).catch(function (err) {
-  console.log('Connection to MongoDB failed.');
-  console.error(err);
-});
-
-
-// Email transporter setup and verification.
-
-var mailer = nodemailer.createTransport(local.smtp);
-
-mailer.verify(function (err) {
-  if (err) {
-    console.log('Connection to mail server failed:');
-    console.log(err);
-  } else {
-    console.log('Connected to mail server...');
+mongoClient.connect(mongoUrl, function (dbErr, db) {
+  if (dbErr) {
+    console.error('Failed to connect to MongoDB.');
+    console.error(dbErr);
+    return;
   }
-});
+  // Success
+  console.log('Connected to MongoDB...');
 
 
-// Start the server.
+  // Email transporter setup and verification.
 
-server.listen(local.port, function () {
-  console.log('Express listening on port ' + local.port + '...');
-});
+  var mailer = nodemailer.createTransport(local.smtp);
 
-
-// Security best practices
-// -----------------------
-// https://expressjs.com/en/advanced/best-practice-security.html
-
-// Do not tell we are using Express.
-app.disable('x-powered-by');
-
-// -----------------------
-// Security best practices END
+  mailer.verify(function (err) {
+    if (err) {
+      console.log('Connection to mail server failed:');
+      console.log(err);
+    } else {
+      console.log('Connected to mail server...');
+    }
+  });
 
 
-// Request logging
-// ---------------
-if (local.env === 'production') {
-  app.use(loggers.http());
-}
-// ---------------
-// Request logging END
+  // Start the server.
+
+  server.listen(local.port, function () {
+    console.log('Express listening on port ' + local.port + '...');
+  });
 
 
-// Static assets
-// -------------
-if (local.env === 'development' || local.env === 'test') {
-  // Webpack development middleware
-  //
-  // The following middleware is only for development.
-  // It serves the static file assets on publicPath in a manner similar
-  // to express.static(publicPath). It also watches the assets for changes and
-  // compiles them on change on background.
-  //
-  // To serve static files in production, use:
-  //     app.use(express.static('./.tmp/public'));
-  // To compile assets for production, run webpack from the command line:
-  //     $ webpack --config ./config/webpack.js
+  // Security best practices
+  // -----------------------
+  // https://expressjs.com/en/advanced/best-practice-security.html
 
-  // eslint-disable-next-line global-require
-  var webpackMiddleware = require('webpack-dev-middleware');
+  // Do not tell we are using Express.
+  app.disable('x-powered-by');
 
-  app.use(webpackMiddleware(webpack(webpackConfig), {
-    // publicPath is required. Use same as in webpackConfig.
-    // See https://github.com/webpack/webpack-dev-middleware
-    publicPath: local.staticUrl,
-    noInfo: true,
-    stats: { colors: true },
-  }));
-  console.log('Webpack listening for file changes...');
+  // -----------------------
+  // Security best practices END
 
-} else {
-  // In production, we run webpack once and serve the files.
-  // The first run builds the initial set of files. The subsequent
-  // runs append to that. Thus, serving static files before the build
-  // finishes is not a problem.
-  console.log('Building static assets...');
-  webpack(webpackConfig, function (err, stats) {
-    if (err || stats.hasErrors()) {
-      if (err) {
-        throw err;
+
+  // Request logging
+  // ---------------
+  if (local.env === 'production') {
+    app.use(loggers.http());
+  }
+  // ---------------
+  // Request logging END
+
+
+  // Static assets
+  // -------------
+  if (local.env === 'development' || local.env === 'test') {
+    // Webpack development middleware
+    //
+    // The following middleware is only for development.
+    // It serves the static file assets on publicPath in a manner similar
+    // to express.static(publicPath). It also watches the assets for changes and
+    // compiles them on change on background.
+    //
+    // To serve static files in production, use:
+    //     app.use(express.static('./.tmp/public'));
+    // To compile assets for production, run webpack from the command line:
+    //     $ webpack --config ./config/webpack.js
+
+    // eslint-disable-next-line global-require
+    var webpackMiddleware = require('webpack-dev-middleware');
+
+    app.use(webpackMiddleware(webpack(webpackConfig), {
+      // publicPath is required. Use same as in webpackConfig.
+      // See https://github.com/webpack/webpack-dev-middleware
+      publicPath: local.staticUrl,
+      noInfo: true,
+      stats: { colors: true },
+    }));
+    console.log('Webpack listening for file changes...');
+
+  } else {
+    // In production, we run webpack once and serve the files.
+    // The first run builds the initial set of files. The subsequent
+    // runs append to that. Thus, serving static files before the build
+    // finishes is not a problem.
+    console.log('Building static assets...');
+    webpack(webpackConfig, function (err, stats) {
+      if (err || stats.hasErrors()) {
+        if (err) {
+          throw err;
+        }  // else
+        console.log(stats.toString({
+          chunks: true,
+          colors: true,
+        }));
+        throw new Error('Error when building static assets.');
       }  // else
+      if (stats.hasWarnings()) {
+        console.log('Built static assets with warnings.');
+      } else {
+        console.log('Built static assets successfully.');
+      }
       console.log(stats.toString({
-        chunks: true,
+        chunks: false,
         colors: true,
       }));
-      throw new Error('Error when building static assets.');
-    }  // else
-    if (stats.hasWarnings()) {
-      console.log('Built static assets with warnings.');
-    } else {
-      console.log('Built static assets successfully.');
-    }
-    console.log(stats.toString({
-      chunks: false,
-      colors: true,
-    }));
-    // See https://webpack.github.io/docs/node.js-api.html#error-handling
+      // See https://webpack.github.io/docs/node.js-api.html#error-handling
+    });
+    app.use(local.staticUrl, express.static(local.staticDir));
+    console.log('Serving static files from', local.staticUrl);
+  }
+  // -------------
+  // Static assets END
+
+
+  // Uploaded files
+  // --------------
+  app.use(local.uploadUrl, express.static(local.uploadDir));
+  console.log('Serving uploaded files from', local.uploadUrl);
+  // --------------
+  // Uploaded files END
+
+
+  // Catch all to single page app.
+  // Must be the final step in the app middleware chain.
+  app.get('/*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'index.html'));
   });
-  app.use(local.staticUrl, express.static(local.staticDir));
-  console.log('Serving static files from', local.staticUrl);
-}
-// -------------
-// Static assets END
 
 
-// Uploaded files
-// --------------
-app.use(local.uploadUrl, express.static(local.uploadDir));
-console.log('Serving uploaded files from', local.uploadUrl);
-// --------------
-// Uploaded files END
+  // Socket.io routing
+
+  io.on('connection', function (socket) {
+
+    // Domain name is required by some handlers, for example
+    // when a link is sent via email. It does not matter if
+    // the connection is transported via polling or websockets,
+    // the host stays the same.
+    var host = socket.request.headers.host;
+
+    console.log('New connection from app hosted at', host);
+
+    return routes(socket, db, mailer, host);
+  });
 
 
-// Catch all to single page app.
-// Must be the final step in the app middleware chain.
-app.get('/*', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '..', 'client', 'index.html'));
+  // Populate database
+
+  bootstrap(db);
 });
-
-
-// Socket.io routing
-
-io.on('connection', function (socket) {
-
-  // Domain name is required by some handlers, for example
-  // when a link is sent via email. It does not matter if
-  // the connection is transported via polling or websockets,
-  // the host stays the same.
-  var host = socket.request.headers.host;
-
-  console.log('New connection from app hosted at', host);
-
-  return routes(socket, db, mailer, host);
-});
-
-
-// Populate database
-
-bootstrap(db);
