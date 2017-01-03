7fd64ede6bd24def73510039c1eecdddb2059f18
Akseli Palén <akseli.palen@gmail.com>
2017-01-03 21:20:02 +0200

    loadsample: include also config and users collections

--- cli/fixtures/sample.js
+++ cli/fixtures/sample.js
@@ -1,10 +1,18 @@
 /* eslint-disable no-magic-numbers, no-sync */
 
-var mongodb = require('mongodb');
-var ObjectId = mongodb.ObjectId;
+var local = require('../../config/local');
+var bcrypt = require('bcryptjs');
+var ObjectId = require('mongodb').ObjectId;
 
 module.exports = {
   collections: {
+    config: [
+      {
+        _id: new ObjectId('58092312bbba430a35fb4139'),
+        key: 'schemaVersion',
+        value: 4,
+      },
+    ],
     locations: [
       {
         _id: new ObjectId('581f166110a1482dd0b7cd13'),
@@ -16,19 +24,30 @@ module.exports = {
         deleted: false,
         tags: ['walk-in'],
         content: [{
+          _id: 'rkblRPFrg',
           type: 'created',
-          user: 'admin',
+          user: local.admin.username,
           time: '2009-07-30T10:44:58.000Z',
           data: {},
         }, {
+          _id: 'rkMxRvKSl',
           type: 'story',
-          user: 'admin',
+          user: local.admin.username,
           time: '2009-09-04T23:44:21.000Z',
           data: {
             markdown: 'A ghost town',
           },
         }],
         layer: 1,
+      },
+    ],
+    users: [
+      {
+        _id: new ObjectId('5867bdf00a5a9e18d7755e4f'),
+        name: local.admin.username,
+        email: local.admin.email,
+        hash: bcrypt.hashSync(local.admin.password, local.bcrypt.rounds),
+        admin: true,
       },
     ],
   },
