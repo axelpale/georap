53efa7c9da17591888d4a72c3c8f22e2f0ff81c3
Akseli Palén <akseli.palen@gmail.com>
2017-03-03 13:39:22 +0200

    migration: fix assertFixtureEquals cache bug by cloning

--- migration/lib/assertFixtureEqual.js
+++ migration/lib/assertFixtureEqual.js
@@ -1,9 +1,10 @@
 // A tool to test if a collection is equal to a fixture.
 // Currently this can test only collections with single document.
 
+var db = require('../../server/services/db');
 var fixtures = require('../fixtures');
 var _ = require('lodash');
-var db = require('../../server/services/db');
+var clone = require('clone');
 
 module.exports = function (collectionName, versionTag, callback) {
   // Compares current collection to a same collection in the fixture specified
@@ -30,9 +31,10 @@ module.exports = function (collectionName, versionTag, callback) {
       return callback(err);
     }
 
-    // Compare only firsts
-    var realFirst = fixColl[0];
-    var fixFirst = docs[0];
+    // Compare only firsts.
+    // Clone because otherwise edits affect some cache and yield errs elsewhere
+    var realFirst = clone(fixColl[0]);
+    var fixFirst = clone(docs[0]);
 
     // _id are generated and thus always differ
     delete realFirst._id;
@@ -49,7 +51,9 @@ module.exports = function (collectionName, versionTag, callback) {
     }
 
     // Print out so that we can see the differences.
+    console.log('Data in db:');
     console.log(realFirst);
+    console.log('Data in fixture:');
     console.log(fixFirst);
 
     return callback({
