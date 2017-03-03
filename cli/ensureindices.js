0abc776adf2162c77a208aa15371e2b1e2d55f93
Akseli Palén <akseli.palen@gmail.com>
2017-03-03 23:19:12 +0200

    cli: ensureindices

---
+++ cli/ensureindices.js
@@ -0,0 +1,27 @@
+// Ensures that the db has all the needed indices
+
+var db = require('../server/services/db');
+var tools = require('../specs/tools');
+var fixture = require('./fixtures/sample');
+var clone = require('clone');
+
+db.init(function (dbErr) {
+  if (dbErr) {
+    return console.error('Failed to connect to MongoDB.');
+  }
+
+  // Leave only indices
+  var custom = clone(fixture);
+  custom.collections = {};
+
+  tools.loadFixture(custom, function (err) {
+    if (err) {
+      console.error('Loading sample data failed.');
+      console.error(err);
+    } else {
+      console.log('Indices were created successfully.');
+    }
+
+    db.close();
+  });
+});
