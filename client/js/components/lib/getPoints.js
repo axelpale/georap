1aad375a184aa5a5c0698f988ff5630f2c19bcc1
Akseli Palén <akseli.palen@gmail.com>
2017-03-04 20:34:59 +0200

    client: add location_unproved_visit_created

--- client/js/components/lib/getPoints.js
+++ client/js/components/lib/getPoints.js
@@ -5,6 +5,7 @@ var pointMap = {
   'location_entry_removed_file': -5,
   'location_entry_created_visit': 20,
   'location_entry_removed_visit': -20,
+  'location_unproved_visit_created': 2,
   'location_name_changed': 0,
   'location_geom_changed': 1,
   'location_tags_changed': 2,
