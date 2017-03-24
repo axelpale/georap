95e07003c34ed66e3c23950e5d802709f2482705
Akseli Palén <akseli.palen@gmail.com>
2017-03-24 21:13:37 +0200

    server: remove invalid comment

--- server/api/events/routes.js
+++ server/api/events/routes.js
@@ -4,7 +4,6 @@ var handlers = require('./handlers');
 var express = require('express');
 var router = express.Router();
 
-// Locations
 router.get('/', handlers.getRecent);
 
 module.exports = router;
