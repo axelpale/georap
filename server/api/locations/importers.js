13384a1f3271f747f84312090ee8b0b646f082b1
Akseli Palen <akseli.palen@gmail.com>
2017-10-11 17:57:51 +0300

    v8.1.0-alpha.4 refactor kml import

--- server/api/locations/importers.js
+++ server/api/locations/importers.js
@@ -1,5 +1,7 @@
-/* eslint-disable max-statements */
-var sax = require('sax');
+/* eslint-disable max-statements,max-lines */
+
+var xmltransform = require('camaro');
+
 
 exports.readKML = function (buffer, callback) {
   // Find an array of locations from a KML file.
@@ -14,251 +16,83 @@ exports.readKML = function (buffer, callback) {
   // - descriptions and GroundOverlays in a Folder are attached under
   //   the location created for the first Placemark within
   //   the Folder.
-  // -
-
-  var strict = true;
-  var parser = sax.parser(strict);
-
-  // Finished locations are collected here. They have structure:
-  // - String name
-  // - Float latitude
-  // - Float longitude
-  // - Array descriptions
-  // - Array overlays
-  var finalLocations = [];
-  // Descriptions and GroundOverlays are collected here.
-  // At Folder close, they are attached to the first location,
-  // and then the locations considered as finished and pushed
-  // into 'locations'.
-  // An item object in 'folders' has following structure:
-  // - descriptions: array
-  // - groudOverlays: array
-  // - locations: array
-  // Var 'folders' is used as a stack because folder hierarchy
-  //   folders[0] to access top item
-  //   folders.shift() to remove top item
-  //   folders.unshift(item) to add top item
-  // We init folders with a root-folder. We need to extract this to
-  // finalLocations at end.
-  var folders = [{
-    descriptions: [],
-    groundOverlays: [],
-    locations: [],
-  }];
-  // Currently processed location is built here.
-  // After location is successfully built, it is added to locations array
-  // and loc is set back to null.
-  var loc = null;
-
-  var onFolderOpen = function () {
-    folders.unshift({
-      descriptions: [],
-      groundOverlays: [],
-      locations: [],
-    });
+  //
+  var placemarkTemplate = {
+    name: 'name',
+    description: 'description',
+    coordinates: 'Point/coordinates',
   };
-  var onFolderClose = function () {
-    var st;
-    var folder = folders.shift();  // remove and process the folder
 
-    // Move collected descriptions and groundOverlays into the first location
-    // before combining folder's accumulated locations into final loc set.
+  var overlayTemplate = {
+    name: 'name',
+    description: 'description',
+    href: './Icon/href',
+    viewBoundScale: 'number(./Icon/viewBoundScale)',
+    latLonBox: {
+      north: 'number(./LatLonBox/north)',
+      south: 'number(./LatLonBox/south)',
+      east: 'number(./LatLonBox/east)',
+      west: 'number(./LatLonBox/west)',
+      rotation: 'number(./LatLonBox/rotation)',
+    },
+  };
+
+  var template = {
+    rootLocations: ['//Document/Placemark', placemarkTemplate],
+    folders: ['//Folder', {
+      description: 'description',
+      locations: ['./Placemark', placemarkTemplate],
+      overlays: ['./GroundOverlay', overlayTemplate],
+    }],
+  };
+
+  var result = xmltransform(buffer.toString('utf8'), template);
+
+  // Combine locations
+  var finalLocations = result.rootLocations;
+  result.folders.forEach(function (folder) {
+
     if (folder.locations.length > 0) {
-      st = folder.locations[0];
-      st.descriptions = st.descriptions.concat(folder.descriptions);
-      st.groundOverlays = st.groundOverlays.concat(folder.groundOverlays);
+      var first = folder.locations[0];
 
-      finalLocations = finalLocations.concat(folder.locations);
-    } else if (folder.descriptions.length > 0) {
-      console.log('Descriptions discarded');
-    } else if (folder.groundOverlays.length > 0) {
-      console.log('GroundOverlays discarded');
+      first.descriptions = [folder.description];
+      first.overlays = folder.overlays;
     }
 
-  };
-  var onFolderDescriptionClose = function (desc) {
-    folders[0].descriptions.push(desc);
-  };
+    finalLocations = finalLocations.concat(folder.locations);
+  });
 
-  var onPlacemarkOpen = function () {
-    loc = {
-      descriptions: [],
-      groundOverlays: [],
-    };
-  };
-  var onPlacemarkClose = function () {
-    if (loc.hasOwnProperty('name') &&
-        loc.hasOwnProperty('longitude') &&
-        loc.hasOwnProperty('latitude')) {
-      folders[0].locations.push(loc);
+  // Format locations: remove empty properties
+  finalLocations.forEach(function (loc) {
+
+    // Combine descriptions
+    if (loc.hasOwnProperty('descriptions')) {
+      loc.descriptions.push(loc.description);
     } else {
-      console.log('Placemark discarded');
+      loc.descriptions = [loc.description];
     }
-    loc = null;
-  };
-  var onPlacemarkNameClose = function (name) {
-    loc.name = name;
-  };
-  var onPlacemarkDescriptionClose = function (desc) {
-    loc.descriptions.push(desc);
-  };
-  var onPlacemarkPointCoordinatesClose = function (coords) {
-    var parts;
+    delete loc.description;
 
-    if (typeof coords === 'string') {
-      parts = coords.split(',');
+    // Remove empty descriptions
+    loc.descriptions = loc.descriptions.filter(function (desc) {
+      return desc.length > 0;
+    });
 
-      if (parts.length >= 2) {
-        loc.longitude = parts[0];
-        loc.latitude = parts[1];
-        return;  // success
-      }
+    // Split coordinates
+    var lonLat = loc.coordinates.split(',');
+    loc.longitude = parseFloat(lonLat[0]);
+    loc.latitude = parseFloat(lonLat[1]);
+    delete loc.coordinates;
+
+    // Always have overlays even when empty
+    if (!loc.hasOwnProperty('overlays')) {
+      loc.overlays = [];
     }
+  });
 
-    // else
-    throw new Error('invalid coordinates');
-  };
-
-  var onEnd = function () {
-    onFolderClose();  // process root-folder
-    return callback(null, finalLocations);
-  };
-
-
-  (function defineParsing() {
-    var text = null;  // combine text and cdata nodes here
-    //var folderTag = false;
-    var placemarkTag = false;
-    //var placemarkNameTag = false;
-    //var placemarkDescriptionTag = false;
-    var placemarkPointTag = false;
-    //var placemarkPointCoordinatesTag = false;
-
-    parser.onerror = function (err) {
-      console.log('ERROR', err);
-    };
-    parser.ontext = function (t) {
-      if (text) {
-        text += t;
-      } else {
-        text = t;
-      }
-    };
-    parser.oncdata = function (a) {
-      console.log('oncdata:', a);
-      if (text) {
-        text += a;
-      } else {
-        text = a;
-      }
-    };
-    parser.onopentag = function (node) {
-      // opened a tag.  node has "name" and "attributes"
-      console.log('opentag:', node);
-      var tag = node.name;
-
-      if (tag === 'Folder') {
-        //folderTag = true;
-        onFolderOpen();
-        return;
-      }
-
-      if (tag === 'Placemark') {
-        placemarkTag = true;
-        onPlacemarkOpen();
-        return;
-      }
-
-      if (placemarkTag) {
-        if (tag === 'name') {
-          //placemarkNameTag = true;
-          text = null;
-          return;
-        }
-
-        if (tag === 'description') {
-          //placemarkDescriptionTag = true;
-          text = null;
-          return;
-        }
-
-        if (tag === 'Point') {
-          placemarkPointTag = true;
-          return;
-        }
-
-        if (placemarkPointTag && tag === 'coordinates') {
-          //placemarkPointCoordinatesTag = true;
-          text = null;
-          return;
-        }
-      }
-
-      if (!placemarkTag && tag === 'description') {
-        // folderDescriptionTag = true;
-        text = null;
-        return;
-      }
-    };
-    parser.onclosetag = function (tag) {
-      console.log('closetag:', tag);
-
-      if (tag === 'Folder') {
-        //folderTag = false;
-        onFolderClose();
-        return;
-      }
-
-      if (tag === 'Placemark') {
-        placemarkTag = false;
-        onPlacemarkClose();
-        return;
-      }
-
-      if (placemarkTag) {
-        if (tag === 'name') {
-          //placemarkNameTag = false;
-          onPlacemarkNameClose(text);
-          return;
-        }
-
-        if (tag === 'description') {
-          //placemarkDescriptionTag = false;
-          onPlacemarkDescriptionClose(text);
-          return;
-        }
-
-        if (tag === 'Point') {
-          placemarkPointTag = false;
-          return;
-        }
-
-        if (placemarkPointTag && tag === 'coordinates') {
-          //placemarkPointCoordinatesTag = false;
-          onPlacemarkPointCoordinatesClose(text);
-          return;
-        }
-      }
-
-      if (!placemarkTag && tag === 'description') {
-        onFolderDescriptionClose(text);
-        return;
-      }
-    };
-    // parser.onattribute = function (attr) {
-    //   // an attribute.  attr has "name" and "value"
-    //   console.log('attr:', attr);
-    // };
-    parser.onend = function () {
-      // parser stream is done, and ready to have more stuff written to it.
-      // console.log('end');
-      onEnd();
-    };
-
-    parser.write(buffer).close();
-  }());
-
+  return callback(null, finalLocations);
 };
+
 
 exports.readKMZ = function (buffer, callback) {
   // KMZ is a zipped collection of resources and KML files.
@@ -266,6 +100,7 @@ exports.readKMZ = function (buffer, callback) {
   // dummy
   return exports.readKMZ(buffer, callback);
 };
+
 
 exports.readGPX = function (buffer, callback) {
   // Find an array of locations from a GPX file.
