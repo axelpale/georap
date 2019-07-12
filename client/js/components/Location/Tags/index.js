6092f6fda1242d1fd5dcdc829fb4462e662f1f46
Akseli Palen <akseli.palen@gmail.com>
2019-07-13 01:51:44 +0300

    Rewrite location status & type selector

--- client/js/components/Location/Tags/index.js
+++ client/js/components/Location/Tags/index.js
@@ -1,21 +1,56 @@
 var tags = require('../../../stores/tags');
 var template = require('./template.ejs');
 var tagsListTemplate = require('./tagsList.ejs');
+var statusFormListTemplate = require('./statusFormList.ejs');
 var tagsFormListTemplate = require('./tagsFormList.ejs');
 
+var allStatusTags = [
+  'walk-in',
+  'active',
+  'buried',
+  'demolished',
+  'guarded',
+  'locked',
+];
+var allTypeTags = tags.getTagsNotIn(allStatusTags);
+
+var isTypeTag = function (t) {
+  return allTypeTags.indexOf(t) !== -1;
+};
+var isStatusTag = function (t) {
+  return allStatusTags.indexOf(t) !== -1;
+};
+
 module.exports = function (location) {
+  var self = this;
 
   this.bind = function ($mount) {
+    var locTags = location.getTags();
+    var locTypeTags = locTags.filter(isTypeTag);
+    var locStatusTags = locTags.filter(isStatusTag);
+
+    var isDemolished = (locStatusTags.indexOf('demolished') !== -1);
 
     $mount.html(template({
-      tagsListHtml: tagsListTemplate({ tags: location.getTags() }),
+      isDemolished: isDemolished,
+      tagsListHtml: tagsListTemplate({
+        isDemolished: isDemolished,
+        typeTags: locTypeTags,
+        statusTags: locStatusTags,
+        tagToSymbolUrl: tags.tagToSymbolUrl,
+      }),
+      statusFormListHtml: statusFormListTemplate({
+        allStatusTags: allStatusTags,
+        statusTags: locStatusTags,
+        tagToSymbolUrl: tags.tagToSymbolUrl,
+      }),
       tagsFormListHtml: tagsFormListTemplate({
-        allTags: tags.getAllTags(),
-        locationTags: location.getTags(),
+        allTags: allTypeTags,
+        typeTags: locTypeTags,
+        tagToSymbolUrl: tags.tagToSymbolUrl,
       }),
     }));
 
-    var $list = $('#tresdb-location-edit-tags-list');
     var $show = $('#tresdb-location-edit-tags-show');
     var $form = $('#tresdb-location-edit-tags-form');
     var $cancel = $('#tresdb-location-edit-tags-cancel');
@@ -23,16 +58,8 @@ module.exports = function (location) {
     var $error = $('#tresdb-location-edit-tags-error');
 
     location.on('location_tags_changed', function () {
-      // Update tags list
-      var tagsListHtml = tagsListTemplate({ tags: location.getTags() });
-      $('#tresdb-location-tags-list').html(tagsListHtml);
-
-      // Update form
-      var tagsFormListHtml = tagsFormListTemplate({
-        allTags: tags.getAllTags(),
-        locationTags: location.getTags(),
-      });
-      $list.html(tagsFormListHtml);
+      $mount.empty();
+      self.bind($mount);
     });
 
     $show.click(function (ev) {
@@ -55,23 +82,11 @@ module.exports = function (location) {
       $form.addClass('hidden');
     });
 
-    $form.submit(function (ev) {
-      ev.preventDefault();
-
-      var newTags = [];
-
-      // See http://stackoverflow.com/a/590040/638546
-      var q = '#tresdb-location-edit-tags-form ' +
-              'input:checkbox[name=tags]:checked';
-      $(q).each(function () {
-        newTags.push($(this).val());
-      });
-
+    var formSubmit = function (newTags) {
       $progress.removeClass('hidden');
       $form.addClass('hidden');
 
       location.setTags(newTags, function (err) {
-
         $progress.addClass('hidden');
 
         if (err) {
@@ -82,8 +97,37 @@ module.exports = function (location) {
         }
         // Everything ok
       });
+    };
+
+    $('#tresdb-tag-default').click(function (ev) {
+      ev.preventDefault();
+      var newTags = locStatusTags.slice();
+      formSubmit(newTags);
     });
 
+    allTypeTags.forEach(function (tag) {
+      $('#tresdb-tag-' + tag).click(function (ev) {
+        ev.preventDefault();
+        var newTags = locStatusTags.slice();
+        newTags.push(tag);
+        formSubmit(newTags);
+      });
+    });
+
+    $('#tresdb-tag-unknown').click(function (ev) {
+      ev.preventDefault();
+      var newTags = locTypeTags.slice();
+      formSubmit(newTags);
+    });
+
+    allStatusTags.forEach(function (tag) {
+      $('#tresdb-tag-' + tag).click(function (ev) {
+        ev.preventDefault();
+        var newTags = locTypeTags.slice();
+        newTags.push(tag);
+        formSubmit(newTags);
+      });
+    });
   };
 
   this.unbind = function () {
