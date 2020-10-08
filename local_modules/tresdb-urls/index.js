7e1c1c05eb02e1ffe933060c1e82fe6deb38fa75
Akseli Palen <akseli.palen@gmail.com>
2020-10-08 22:45:22 +0300

    Create tresdbUrls.iconUrl

--- local_modules/tresdb-urls/index.js
+++ local_modules/tresdb-urls/index.js
@@ -2,3 +2,8 @@ exports.locationTypeToSymbolUrl = function (type) {
   var baseUrl = '/assets/images/markers/symbols/';
   return baseUrl + type + '.png';
 };
+
+exports.iconUrl = function (templateName, symbolName) {
+  var iconName = templateName + '-' + symbolName + '.png';
+  return '/api/icons/' + iconName;
+};
