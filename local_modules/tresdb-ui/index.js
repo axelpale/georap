89db508bdd267e573952a619c6e6cfc79eda6d01
Akseli Palen <akseli.palen@gmail.com>
2020-12-20 21:42:57 +0200

    ui.flash to flash elements in color

--- local_modules/tresdb-ui/index.js
+++ local_modules/tresdb-ui/index.js
@@ -13,3 +13,25 @@ exports.hide = function ($el) {
 exports.toggleHidden = function ($el) {
   $el.toggleClass('hidden');
 };
+
+exports.flash = function ($el, color) {
+  // Change element background color temporarily.
+  // Useful to highlight things.
+  //
+  // Parameters:
+  //   $el: jQuery element
+  //   color: a hex color string
+  //
+  var DURATION = 2;
+  var DELAY = 2;
+  var SECOND = 1000;
+  var original = $el.css('background-color');
+  $el.css('background-color', color);
+  $el.css('transition', 'background-color ' + DURATION + 's');
+  window.setTimeout(function () {
+    $el.css('background-color', original);
+  }, DELAY * SECOND);
+  window.setTimeout(function () {
+    $el.css('transition', 'unset');
+  }, (DURATION + DELAY) * SECOND);
+}
