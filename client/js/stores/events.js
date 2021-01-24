f35511c5755a33cd01f97b229e533a11b4b66199
Akseli Palen <akseli.palen@gmail.com>
2021-01-24 17:55:40 +0200

    Remove unnecessarily complex event caching

--- client/js/stores/events.js
+++ client/js/stores/events.js
@@ -20,15 +20,9 @@ var account = require('./account');
 
 emitter(exports);
 
-// The cache includes s most recent events in time order, most recent first.
-// Cache builds up in two directions:
-// - new events via socket grow head
-// - user wants to see the older ones -> tail grows
-var _cache = [];
-
-// Listening for new events.
+// Listen for new events and inform views that
+// new events are available.
 socket.on('tresdb_event', function (ev) {
-  _cache.unshift(ev);
   exports.emit('events_changed');
 });
 
@@ -73,51 +67,12 @@ exports.getRecent = function (n, callback) {
   //         events
   //           array, most recent event first
 
-  var last;
+  var beforeTime = null; // null equals now
 
-  var s = _cache.length;
-  var beforeTime = null;
-
-  // Init number of items to fetch.
-  var q = 0;
-
-  // Check cache if n most recent already exists there.
-  if (s > 0) {
-    if (s >= n) {
-      return callback(null, _cache.slice(0, n));
-    }
-    // Assert: cache is not empty but does not include enough items.
-    // We must request n - s items, where s is the cache size.
-    // The items must be from the time before the last cached item.
-    q = n - s;
-    last = _cache[s - 1];
-    beforeTime = last.time;
-  } else {
-    q = n;
-  }
-
-  // Cache does not contain enough items so we fetch them.
-  fetch(q, beforeTime, function (err, items) {
+  fetch(n, beforeTime, function (err, items) {
     if (err) {
       return callback(err);
     }
-
-    // Update cache. Append to array.
-    // Assert: the resulting array is in correct time order, oldest last.
-    _cache = _cache.concat(items);
-
-    // Inform views or other listeners that events have been changed.
-    if (items.length > 0) {
-      exports.emit('events_changed');
-    }
-
-    // Now cache should contain enough items.
-    // Otherwise there is not enough existing items.
-    if (_cache.length < n) {
-      // Return all items there is.
-      return callback(null, _cache);
-    }
-    // Return N most recent items.
-    return callback(null, _cache.slice(0, n));
+    return callback(null, items);
   });
 };
