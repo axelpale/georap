c4231a5f43e6e30f0f56ad79e1757a3aba33ac01
Akseli Palen <akseli.palen@gmail.com>
2021-02-04 02:00:04 +0200

    Update thumbnail on entry change

--- client/js/components/Location/Thumbnails/Thumbnail/index.js
+++ client/js/components/Location/Thumbnails/Thumbnail/index.js
@@ -1,4 +1,5 @@
 var template = require('./template.ejs');
+var ui = require('tresdb-ui');
 
 module.exports = function (entry) {
   // Parameters:
@@ -7,6 +8,8 @@ module.exports = function (entry) {
 
   var self = this;
 
+  var _changeHandler = null;
+
   // Public methods
 
   this.bind = function ($mount) {
@@ -14,14 +17,22 @@ module.exports = function (entry) {
       entry: entry,
     }));
 
-    entry.on('location_entry_changed', function () {
-      // Rebind
-      self.unbind();
-      self.bind($mount);
-    });
+    _changeHandler = function (ev) {
+      // If entry still has an image, replace the image.
+      if (entry.hasImage()) {
+        var $img = $mount.find('img');
+        $img.attr('src', entry.getThumbUrl());
+        $img.attr('title', entry.getFileName());
+      } else {
+        // No image anymore. Hide.
+        ui.hide($mount);
+      }
+    };
+
+    entry.on('location_entry_changed', _changeHandler);
   };
 
   this.unbind = function () {
-    entry.off();
+    entry.off('location_entry_changed', _changeHandler);
   };
 };
