2bb22ed2688663b8d6157b6505d7c68a899683c1
Akseli Palen <akseli.palen@gmail.com>
2021-02-07 18:37:22 +0200

    Hide excess entries and display Show more -button

--- client/js/components/Location/Entries/index.js
+++ client/js/components/Location/Entries/index.js
@@ -11,12 +11,18 @@ module.exports = function (entries) {
   var _handleEntryCreated;
   var _handleEntryRemoved;
 
+  // Keep track how many of the given entries are visible.
+  // Do not count added or removed entries; exact number of rendered
+  // entries does not matter, only the positions in the given entries.
+  var _maxVisible = 10; // Current ceiling. Can be increased by more button.
+  var _numberVisible = 0; // Num of given entries appended.
+  var _stepVisible = 10; // How much maxVisible will be grown per more click.
 
   this.bind = function ($mount) {
-
     var ents = entries.toArray();
+    var appendEntry, appendShowMore, appendEntries;
 
-    ents.forEach(function (e) {
+    appendEntry = function (e) {
       var id = e.getId();
       var v = new EntryView(e);
 
@@ -24,7 +30,36 @@ module.exports = function (entries) {
 
       $mount.append('<div id="' + id + '"></div>');
       v.bind($('#' + id));
-    });
+    };
+
+    appendShowMore = function () {
+      $mount.append('<button id="tresdb-location-entries-more" ' +
+        'class="btn btn-primary btn-block" type="button">' +
+        '<span class="glyphicon glyphicon-chevron-down" aria-hidden="true">' +
+        '</span> ' +
+        'Show more posts</button>');
+      var $more = $('#tresdb-location-entries-more');
+      $more.click(function () {
+        $more.remove();
+        _maxVisible += _stepVisible;
+        appendEntries();
+      });
+    };
+
+    appendEntries = function () {
+      var appendable = ents.slice(_numberVisible, _maxVisible);
+      for (var i = 0; i < appendable.length; i += 1) {
+        appendEntry(appendable[i]);
+        _numberVisible += 1;
+      }
+      // If hidden entries still remain, show more button
+      if (_numberVisible < ents.length) {
+        appendShowMore();
+      }
+    };
+
+    // Initial visible entries
+    appendEntries();
 
     _handleEntryCreated = function (ev) {
       // Create view and store it among others
