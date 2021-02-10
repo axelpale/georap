072b47c9d853bb59d4161a10a8dabecc82fd0163
Akseli Palen <akseli.palen@gmail.com>
2021-02-10 14:38:08 +0200

    New local module: tresdb-key

---
+++ local_modules/tresdb-key/README.md
@@ -0,0 +1,7 @@
+# tresdb-key
+
+Generate short and usable id-like keys.
+
+    > const key = require('tresdb-key');
+    > key.generate()
+    'xdrCieRMfJ'
---
+++ local_modules/tresdb-key/index.js
@@ -0,0 +1,7 @@
+const nanoid = require('nanoid');
+const nolookalikes = require('nanoid-dictionary/nolookalikes');
+const generate = nanoid.customAlphabet(nolookalikes, 10);
+
+exports.generate = function () {
+  return generate();
+};
