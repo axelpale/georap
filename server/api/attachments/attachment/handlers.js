cb6ea133f68019ef02ee05ed6c868ea1dd5ceab1
Akseli Palen <akseli.palen@gmail.com>
2021-02-10 22:05:29 +0200

    Sketchy rotateImage

--- server/api/attachments/attachment/handlers.js
+++ server/api/attachments/attachment/handlers.js
@@ -1,48 +1,54 @@
 
-var templates = require('./templates');
 var dal = require('./dal');
-var loggers = require('../../../services/logs/loggers');
-var config = require('tresdb-config');
 var status = require('http-status-codes');
-var sanitizeFilename = require('sanitize-filename');
-var slugify = require('slugify');
-var uploads = require('../../../../services/uploads');
-var entriesDal = require('../../../entries/dal');
-var uploadHandler = uploads.uploader.single('entryfile');
+var uploads = require('../../../services/uploads');
 
-exports.get = function (req, res, next) {
-  // Fetch single attachment
+exports.get = function (req, res) {
+  // Fetch single attachment.
+  // Already feched but here it is a good place to strip any unnecesary data.
   return res.json(req.attachment);
 };
 
-exports.rotatePhoto = function (req, res, next) {
+exports.rotateImage = function (req, res, next) {
   // Try to rotate photo attachment.
 
-  var atta = req.attachment;
+  const atta = req.attachment;
 
   // Detect if image
+  // eslint-disable-next-line no-magic-numbers
+  if (atta.mimetype.substr(0, 6) !== 'image/') {
+    // No file attachment
+    const msg = 'Cannot rotate ' + atta.mimetype;
+    return res.status(status.BAD_REQUEST).send(msg);
+  }
+
+  // Positive for clockwise rotation.
+  const degrees = parseInt(req.body.degrees, 10);
+  if (isNaN(degrees)) {
+    const msg = 'Invalid degree ' + degrees;
+    return res.status(status.BAD_REQUEST).send(msg);
+  }
+
+  // If 0 degree, no need to rotate.
+  // eslint-disable-next-line no-magic-numbers
+  if (degrees % 360 === 0) {
+    return res.send('Skipped unnecessary rotation of 0deg');
+  }
 
   // Rotate
-  // TODO var imageFile = path ?
-  // TODO sharp
-
-  // After rotate, create thumbnail.
-  // Create even for non-images.
-  uploads.createThumbnail(imageFile, function (errt, thumb) {
-    if (errt) {
-      return next(errt);
+  uploads.rotateImage(atta.filepath, degrees, (err) => {
+    if (err) {
+      return next(err);
     }
-
-    dal.changeAttachment({
-      filepath: uploads.getRelativePath(req.file.path),
-      mimetype: req.file.mimetype,
-      thumbfilepath: uploads.getRelativePath(thumb.path),
-      thumbmimetype: thumb.mimetype,
-    }, function (err) {
-      if (err) {
-        return next(err);
+    // After rotate, recreate thumbnail.
+    uploads.createThumbnail({
+      path: atta.filepath,
+      mimetype: atta.mimetype,
+    }, (errt) => {
+      if (errt) {
+        return next(errt);
       }
-      return res.sendStatus(status.OK);
+      return res.send('Rotated ' + degrees + 'deg');
     });
   });
 };
