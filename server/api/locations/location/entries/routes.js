befcf8347971378ba72b8d6b9c97c5eb7b2b484f
Akseli Palen <akseli.palen@gmail.com>
2021-02-10 17:34:40 +0200

    Remove unnecessary json parser

--- server/api/locations/location/entries/routes.js
+++ server/api/locations/location/entries/routes.js
@@ -5,7 +5,6 @@ var router = express.Router();
 
 var handlers = require('./handlers');
 var entryIdParser = require('./lib/entryIdParser');
-var jsonParser = require('body-parser').json();
 var commentsRouter = require('./comments/routes');
 
 router.post('/', handlers.create); // uses multer body-parser inside
@@ -13,7 +12,7 @@ router.post('/', handlers.create); // uses multer body-parser inside
 router.use('/:entryId', entryIdParser);
 
 router.post('/:entryId', handlers.change);  // uses multer body-parser inside
-router.delete('/:entryId', jsonParser, handlers.remove);
+router.delete('/:entryId', handlers.remove);
 
 router.use('/:entryId/comments', commentsRouter);
 
