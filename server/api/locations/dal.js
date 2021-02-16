3da00f7208f9b157b7bcd659011e96c3ee3ef93e
Akseli Palen <akseli.palen@gmail.com>
2021-02-16 23:59:41 +0200

    Add missing visits prop from location creation

--- server/api/locations/dal.js
+++ server/api/locations/dal.js
@@ -65,6 +65,7 @@ exports.createLocation = function (args, callback) {
       places: [],
       status: config.locationStatuses[0],
       type: config.locationTypes[0],
+      visits: [],
     };
 
     var coll = db.collection('locations');
