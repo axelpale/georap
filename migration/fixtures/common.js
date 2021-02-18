0ad68f5bff4753707f555dda203489206c708ffa
Akseli Palen <akseli.palen@gmail.com>
2021-02-18 14:41:42 +0200

    Add missingId for tests

--- migration/fixtures/common.js
+++ migration/fixtures/common.js
@@ -7,3 +7,6 @@ exports.PASSWORD = bcrypt.hashSync('admin_password', config.bcrypt.rounds);
 
 exports.irbeneId = db.id('581f166110a1482dd0b7cd13');
 exports.locatorEntryId = db.id('581f166110a1482dd0b7ea02');
+
+// This is a special id. No object with this _id should exist in database.
+exports.missingId = db.id('581f01234567890123456789');
