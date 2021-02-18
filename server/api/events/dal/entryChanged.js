89f24b6089fa3feb3855502ebfd5bff2caf3173d
Akseli Palen <akseli.palen@gmail.com>
2021-02-18 17:39:46 +0200

    Emit location_entry_changed with complete attachments

--- server/api/events/dal/entryChanged.js
+++ server/api/events/dal/entryChanged.js
@@ -1,7 +1,10 @@
 const db = require('tresdb-db');
+const asyn = require('async');
 const lib = require('./lib');
 
 module.exports = (params, callback) => {
+  // Notify about changed entry
+  //
   // Parameters:
   //   params:
   //     entryId
@@ -14,7 +17,7 @@ module.exports = (params, callback) => {
   //       object of changed values
   //     original
   //       object of original values
-
+  //
   if (typeof params.oldEntry._id !== 'object') {
     throw new Error('Invalid entry id type: ' + typeof params.oldEntry._id);
   }
@@ -32,5 +35,60 @@ module.exports = (params, callback) => {
     },
   };
 
-  lib.insertAndEmit(newEvent, callback);
+  // Insert the basic version and emit an extended version
+  // with complete attachments (if needed).
+  lib.insertOne(newEvent, (err, newId) => {
+    if (err) {
+      return callback(err);
+    }
+
+    // Clone and fill id
+    const eventForEmit = Object.assign({}, newEvent, {
+      _id: newId,
+    });
+
+    // Convert attachment keys to attachments.
+    // This prevents additional requests from clients.
+    asyn.waterfall([
+
+      (next) => {
+        if (params.original.attachments) {
+          attachmentsDal.getMany(params.original.attachments, (merr, cats) => {
+            if (merr) {
+              return next(merr);
+            }
+            eventForEmit.data.original.attachments = cats;
+            return next();
+          });
+        } else {
+          return next();
+        }
+      },
+
+      (next) => {
+        if (params.delta.attachments) {
+          attachmentsDal.getMany(params.delta.attachments, (merr, cats) => {
+            if (merr) {
+              return next(merr);
+            }
+            eventForEmit.data.delta.attachments = cats;
+            return next();
+          });
+        } else {
+          return next();
+        }
+      }
+
+    ], (finalErr) => {
+      if (finalErr) {
+        return callback(finalErr);
+      }
+
+      // Emit the extended version.
+      lib.emitOne(eventForEmit);
+
+      // TODO maybe outside waterfall?
+      return callback();
+    });
+  });
 };
