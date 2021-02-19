3b32a2a521f121622f15a28290f53794a7c7d0e4
Akseli Palen <akseli.palen@gmail.com>
2021-02-19 02:45:52 +0200

    Experimental tools for model forwarding

--- local_modules/tresdb-models/index.js
+++ local_modules/tresdb-models/index.js
@@ -9,3 +9,42 @@ exports.rawLocationToMarkerLocation = function (rawLoc) {
     childLayer: rawLoc.layer
   }
 }
+
+exports.drop = function (arr, test) {
+  // Find index
+  var i, index
+  index = -1
+  for (i = 0; i < arr.length; i += 1) {
+    if (test(arr[i])) {
+      index = i
+      break
+    }
+  }
+
+  // Already removed if not found, thus return
+  if (index === -1) {
+    return
+  }
+
+  // Remove in place
+  arr.splice(index, 1)
+}
+
+exports.forward = function (forwarders) {
+  return function (obj, ev) {
+    if (ev.type in forwarders) {
+      forwarders[ev.type](obj, ev)
+    }
+  }
+}
+
+exports.forwardOne = function (model, test) {
+  return function (arr, ev) {
+    const obj = arr.find(function (o) {
+      return test(o, ev)
+    })
+    if (obj) {
+      model.forward(obj, ev)
+    }
+  }
+}
