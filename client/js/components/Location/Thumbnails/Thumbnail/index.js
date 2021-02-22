e80268b5893c95fd09bfc6d43496abf96f9f8457
Akseli Palen <akseli.palen@gmail.com>
2021-02-22 23:22:44 +0200

    Use entryModel.bus

--- client/js/components/Location/Thumbnails/Thumbnail/index.js
+++ client/js/components/Location/Thumbnails/Thumbnail/index.js
@@ -1,15 +1,15 @@
 var template = require('./template.ejs');
 var entryModel = require('../../../Entriex/Entry/model');
 var attachmentModel = require('../../../Attachments/Attachment/model');
-var bus = require('tresdb-bus');
 var ui = require('tresdb-ui');
 
 module.exports = function (entry) {
   // Parameters:
   //   entry
   //     entry object.
-
+  //
   var route = null;
+  var bus = entryModel.bus(entry);
 
   // Public methods
 
@@ -18,24 +18,21 @@ module.exports = function (entry) {
       entry: entry,
     }));
 
-    var route = bus.on(
-      'location_entry_changed',
-      entryModel.handlerFor(entry, function (ev) {
-        // If entry still has an image, replace the image.
-        var firstImage = entryModel.getImage(entry);
-        if (firstImage) {
-          var $img = $mount.find('img');
-          $img.attr('src', attachmentModel.getThumbUrl(firstImage));
-          $img.attr('title', attachmentModel.getFileName(firstImage));
-        } else {
-          // No image anymore. Hide.
-          ui.hide($mount);
-        }
-      })
-    );
+    bus.on('location_entry_changed', function (ev) {
+      // If entry still has an image, replace the image.
+      var firstImage = entryModel.getImage(entry);
+      if (firstImage) {
+        var $img = $mount.find('img');
+        $img.attr('src', attachmentModel.getThumbUrl(firstImage));
+        $img.attr('title', attachmentModel.getFileName(firstImage));
+      } else {
+        // No image anymore. Hide.
+        ui.hide($mount);
+      }
+    });
   };
 
   this.unbind = function () {
-    bus.off(route);
+    bus.off();
   };
 };
