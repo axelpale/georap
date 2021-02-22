e2b81b8a2f557d0780c4eb13c6d1d295b38d6701
Akseli Palen <akseli.palen@gmail.com>
2021-02-23 00:13:51 +0200

    tresdb-models .bus with tests

--- local_modules/tresdb-models/index.js
+++ local_modules/tresdb-models/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-var */
+
 exports.rawLocationToMarkerLocation = function (rawLoc) {
   return {
     _id: rawLoc._id,
@@ -45,6 +47,61 @@ exports.forwardOne = function (model, test) {
     })
     if (obj) {
       model.forward(obj, ev)
+    }
+  }
+}
+
+exports.bus = function (sourceBus, test) {
+  // A super-factory that creates a model-specific bus factory.
+  // Takes a test function that specifies which events are handled.
+  //
+  // Parameters:
+  //   sourceBus
+  //     a minibus bus that will be listened for events.
+  //   test
+  //     function (obj, ev) => bool. If true, event handler will be called.
+  //
+  // Return
+  //   function. A general factory that creates a model-specific bus factory.
+  //
+  // Model-specific bus factory
+  //   function (obj) => object-specific bus instance
+  //
+  // Object-specific bus instance
+  //   object { on, off }, where
+  //     on: function (eventName, handler)
+  //     off: function ()
+  //
+  // Usage
+  //   var busFactory = models.bus((location, ev) => {
+  //     return location._id === ev.locationId
+  //   })
+  //   var bus = busFactory(location)
+  //   bus.on('location_entry_created', (ev) => { ... })
+  //   bus.off()
+  //
+  return function (obj) {
+    // Return a bus object that emits events of the given object.
+    //
+    var routes = []
+    return {
+
+      on: function (evName, handler) {
+        var route = sourceBus.on(evName, function (ev) {
+          if (test(obj, ev)) {
+            return handler(ev)
+          }
+        })
+        routes.push(route)
+      },
+
+      off: function () {
+        routes.forEach(function (route) {
+          sourceBus.off(route)
+        })
+        routes = null // for garbage collector
+      }
+
     }
   }
 }
---
+++ local_modules/tresdb-models/test.js
@@ -0,0 +1,38 @@
+const test = require('tape')
+const models = require('./index')
+const rootBus = require('minibus').create()
+
+test('bus', t => {
+  const busFactory = models.bus(rootBus, (obj, ev) => {
+    return obj.foo === ev.baz && ev.baz === 'bar'
+  })
+
+  const obj = {
+    foo: 'bar'
+  }
+
+  const bus = busFactory(obj)
+
+  let calls = 0
+  bus.on('test-event', function (ev) {
+    calls += 1
+  })
+
+  // Send three test events. Only two should pass the test.
+  rootBus.emit('test-event', {
+    baz: 'bar'
+  })
+  rootBus.emit('test-event', {
+    baz: '666'
+  })
+  rootBus.emit('test-event', {
+    baz: 'bar'
+  })
+  // Fourth event with correct ev but wrong event name
+  rootBus.emit('other-event', {
+    baz: 'bar'
+  })
+
+  t.equal(calls, 2, 'number of calls')
+  t.end()
+})
