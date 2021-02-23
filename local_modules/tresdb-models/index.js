7aa1eb700773e4230d8634be8917e2accba656a0
Akseli Palen <akseli.palen@gmail.com>
2021-02-23 21:08:28 +0200

    Publish the models

--- local_modules/tresdb-models/index.js
+++ local_modules/tresdb-models/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-var */
 
 exports.attachment = require('./attachment')
-
-exports.bus = require('./bus')
+exports.comment = require('./comment')
+exports.comments = require('./comments')
+exports.entries = require('./entries')
+exports.entry = require('./entry')
 
 exports.rawLocationToMarkerLocation = function (rawLoc) {
   return {
