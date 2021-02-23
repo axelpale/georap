4561ff4c762eb142b1d225a236133613abf0f0a9
Akseli Palen <akseli.palen@gmail.com>
2021-02-23 21:44:50 +0200

    Remove unnecessary attachment.getFileName

--- local_modules/tresdb-models/attachment.js
+++ local_modules/tresdb-models/attachment.js
@@ -1,13 +1,3 @@
-/* eslint-disable no-var */
-
-exports.getFileName = function (attachment) {
-  // Get filename part of attachment file path.
-  //
-  // For example if filepath === '/foo/bar/baz.jpg'
-  // then getFileName() === 'baz.jpg'
-  var p = attachment.filepath
-  return p.substr(p.lastIndexOf('/') + 1)
-}
 
 exports.getMimeType = function (attachment) {
   // Return null if no file
