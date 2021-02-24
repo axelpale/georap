6bc999c127e9af126ef541bed4f07f8ded0b2eb2
Akseli Palen <akseli.palen@gmail.com>
2021-02-24 21:24:56 +0200

    Dummy openable entry edit form

--- client/js/components/Location/Entriex/Entry/index.js
+++ client/js/components/Location/Entriex/Entry/index.js
@@ -1,6 +1,7 @@
 var template = require('./template.ejs');
 var entryModel = require('tresdb-models').entry;
 var CommentsView = require('./Comments');
+var FormView = require('../Form');
 var ui = require('tresdb-ui');
 var account = tresdb.stores.account;
 
@@ -12,11 +13,15 @@ module.exports = function (entry) {
 
   var children = {};
 
+  var isAuthor = account.isMe(entry.user);
+  var isAdmin = account.isAdmin();
+  var isAuthorOrAdmin = (isAuthor || isAdmin);
+
   this.bind = function ($mount) {
     $mount.html(template({
       entryId: entry._id,
       username: entry.user,
-      isOwner: (entry.user === account.getName()),
+      isAuthorOrAdmin: isAuthorOrAdmin,
       flagstamp: ui.flagstamp(entry.flags),
       timestamp: ui.timestamp(entry.time),
       hasMarkdown: entryModel.hasMarkdown(entry),
@@ -27,6 +32,15 @@ module.exports = function (entry) {
 
     children.comments = new CommentsView(entry);
     children.comments.bind($mount.find('.entry-comments-container'));
+
+    if (isAuthorOrAdmin) {
+      children.editform = new FormView(entry);
+      children.editform.bind($mount.find('.entry-form-container'));
+
+      $mount.find('.entry-form-open').click(function () {
+        ui.toggleHidden($mount.find('.entry-form-container'));
+      });
+    }
   };
 
   this.unbind = function () {
