bf70a7ae26b2db3867578b1957f56421d7fd4f66
Akseli Palen <akseli.palen@gmail.com>
2021-03-10 15:17:00 +0200

    Make crosshair marker draggable

--- client/js/components/Map/CrosshairMarker/index.js
+++ client/js/components/Map/CrosshairMarker/index.js
@@ -2,11 +2,19 @@
 
 var icons = require('../lib/icons');
 var getVisibleCenter = require('../lib/getVisibleCenter');
+var setVisibleCenter = require('../lib/setVisibleCenter');
 var bus = require('tresdb-bus');
 
 module.exports = function (map) {
 
   var crosshairMarker = null;
+
+  // Keep track if the marker is dragged or not.
+  // This helps to determine should map pan change the marker position or not.
+  // Dragging marker near the edge will pan the view,
+  // which would cause the marker to flick between pointer and center,
+  // if we do not care if marker is dragged or not.
+  var dragged = false;
 
   var showMarker = function () {
     // Reveal the crosshair.
@@ -28,17 +36,35 @@ module.exports = function (map) {
         position: visCenter, // LatLng literal
         map: map,
         icon: icons.crosshair(),
-        draggable: false,
+        draggable: true,
       });
+
       // Initial emit
       setTimeout(function () {
         bus.emit('crosshair_marker_moved', visCenter);
       }, 0);
+
+      // See comment next to var dragged for details.
+      crosshairMarker.addListener('dragstart', function () {
+        dragged = true;
+      });
+
+      crosshairMarker.addListener('dragend', function () {
+        // See comment next to var dragged for details.
+        dragged = false;
+        // Move the map so that the marker is at the middle.
+        // This will fire map idle event which fires crosshair_marker_moved
+        var crosshairLatLng = crosshairMarker.getPosition();
+        // Convert to LatLngLiteral
+        var panTarget = setVisibleCenter(crosshairLatLng.toJSON(), map);
+        // Finally, pan the map.
+        map.panTo(panTarget);
+      });
     }
   };
 
   var updateMarker = function () {
-    if (crosshairMarker) {
+    if (crosshairMarker && !dragged) {
       var visCenter = getVisibleCenter(map);
       crosshairMarker.setPosition(visCenter);
       // HACK A bit hacky way to pass marker position
@@ -51,6 +77,8 @@ module.exports = function (map) {
   var hideMarker = function () {
     // Remove crosshair marker from the map.
     if (crosshairMarker) {
+      google.maps.event.clearListeners(crosshairMarker, 'dragstart');
+      google.maps.event.clearListeners(crosshairMarker, 'dragend');
       crosshairMarker.setMap(null);
       crosshairMarker = null;
     }
