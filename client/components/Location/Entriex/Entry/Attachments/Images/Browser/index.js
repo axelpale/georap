13ed425c4588a0258abfc93109ea8291c9068c6f
Akseli Palen <akseli.palen@gmail.com>
2021-03-17 12:14:19 +0200

    Keyboard photo navigation for multi-image posts

--- client/components/Location/Entriex/Entry/Attachments/Images/Browser/index.js
+++ client/components/Location/Entriex/Entry/Attachments/Images/Browser/index.js
@@ -7,6 +7,8 @@ module.exports = function (attachments) {
   var self = this;
   emitter(self);
 
+  var selectedIndex = 0;
+
   var $elems = {};
 
   this.bind = function ($mount) {
@@ -17,20 +19,55 @@ module.exports = function (attachments) {
 
     $mount.html(template({
       attachments: attachments,
+      selectedIndex: selectedIndex,
     }));
+
+    var selectImage = function (index) {
+      if (index >= 0 && index < attachments.length) {
+        selectedIndex = index;
+        var selectedImage = attachments[selectedIndex];
+        // Emphasize clicked and de-emphasize other images
+        $mount.find('img').each(function (i, el) {
+          if (selectedIndex === i) {
+            el.className = 'active';
+          } else {
+            el.className = '';
+          }
+        });
+        // Allow key events
+        $mount.focus();
+        // Let Viewer know.
+        self.emit('image_selected', selectedImage);
+      }
+    };
+
+    var selectImageByKey = function (key) {
+      var indexCandidate = attachments.findIndex(function (att) {
+        return att.key === key;
+      });
+      if (indexCandidate >= 0) {
+        selectImage(indexCandidate);
+      }
+    };
 
     $mount.on('click', function (ev) {
       var img = ev.target;
       if ('attachmentKey' in img.dataset) {
-        var selectedKey = img.dataset.attachmentKey;
-        var selectedImage = attachments.find(function (att) {
-          return att.key === selectedKey;
-        });
-        self.emit('image_selected', selectedImage);
-        // Emphasize clicked
-        $mount.find('img').removeClass('active');
-        img.className = 'active';
-        img.focus();
+        selectImageByKey(img.dataset.attachmentKey);
+      }
+    });
+
+    // When mount has focus, enable arrow keys for navigation
+
+    var ARROW_LEFT = 37;
+    var ARROW_RIGHT = 39;
+    $mount.keyup(function (ev) {
+      if (ev.which === ARROW_LEFT) {
+        selectImage(selectedIndex - 1);
+      }
+
+      if (ev.which === ARROW_RIGHT) {
+        selectImage(selectedIndex + 1);
       }
     });
   };
