1a7adda6ad10bfb161f32f8922dfef484718f92b
Akseli Palen <akseli.palen@gmail.com>
2021-03-18 21:04:31 +0200

    Make rotateImage to create a new attachment instead of modifying

--- server/api/attachments/attachment/handlers.js
+++ server/api/attachments/attachment/handlers.js
@@ -1,6 +1,5 @@
 
 const dal = require('./dal');
-const lib = require('./lib');
 const urls = require('./urls');
 const status = require('http-status-codes');
 const uploads = require('../../../services/uploads');
@@ -14,17 +13,29 @@ exports.get = (req, res) => {
 
 exports.rotateImage = (req, res, next) => {
   // Try to rotate photo attachment.
+  // This will create a new attachment with the rotated image.
   //
   // Response with JSON
   //   {
   //     attachment: <attachment with full urls>
   //   }
   //
+  // NOTE At first, we tried to rotate images in place, overwriting the orig.
+  // That had two drawbacks:
+  // - identical path does not bust caches
+  // - sharp cannot output image to the source path
+  // Then, we tried rename the rotated file with -rot postfix and
+  // update the paths in the attachment object.
+  // That had one major drawback: the attachment key remained the same
+  // and therefore there was no simple way to cancel the rotation
+  // whenever user cancels the entry edit. Also, rotations would not
+  // be visible in location_entry_changed events if key remains the same.
+  // Therefore, it is better to create a brand new attachment.
+  //
   const atta = req.attachment;
 
   // Detect if image
-  // eslint-disable-next-line no-magic-numbers
-  if (atta.mimetype.substr(0, 6) !== 'image/') {
+  if (!atta.mimetype.startsWith('image/')) {
     // No file attachment
     const msg = 'Cannot rotate ' + atta.mimetype;
     return res.status(status.BAD_REQUEST).send(msg);
@@ -37,50 +48,54 @@ exports.rotateImage = (req, res, next) => {
     return res.status(status.BAD_REQUEST).send(msg);
   }
 
-  // If 0 degree, no need to rotate.
-  // eslint-disable-next-line no-magic-numbers
-  if (degrees % 360 === 0) {
-    return res.send('Skipped unnecessary rotation of 0deg');
+  // If 0 degree, no need to rotate. Response with the same attachment
+  const circle = 360;
+  if (degrees % circle === 0) {
+    return res.json({
+      attachment: atta,
+    });
   }
 
   const sourcePath = uploads.getAbsolutePath(atta.filepath);
 
-  // To bust caches, the new name is an easy way.
-  const targetPath = lib.appendToFilename(sourcePath, '-rot');
-
-  // Rotate
-  uploads.rotateImage(sourcePath, targetPath, degrees, (err) => {
-    if (err) {
-      return next(err);
+  // Prepare directory for a new permanent upload.
+  uploads.preparePermanent(sourcePath, (errp, targetPath) => {
+    if (errp) {
+      return next(errp);
     }
-    // After rotate, recreate thumbnail.
-    uploads.createThumbnail({
-      path: targetPath,
-      mimetype: atta.mimetype,
-    }, (errt, thumb) => {
-      if (errt) {
-        return next(errt);
+
+    // Rotate
+    uploads.rotateImage(sourcePath, targetPath, degrees, (err) => {
+      if (err) {
+        return next(err);
       }
 
-      const updateParams = {
-        key: atta.key,
-        filepath: uploads.getRelativePath(targetPath),
+      // After rotate, recreate thumbnail.
+      uploads.createThumbnail({
+        path: targetPath,
         mimetype: atta.mimetype,
-        thumbfilepath: uploads.getRelativePath(thumb.path),
-        thumbmimetype: thumb.mimetype,
-      };
-
-      // Update attachment
-      dal.change(updateParams, (errc) => {
-        if (errc) {
-          return next(errc);
+      }, (errt, thumb) => {
+        if (errt) {
+          return next(errt);
         }
 
-        const updatedAttachment = Object.assign({}, atta, updateParams);
-        const attachmentWithUrls = urls.complete(updatedAttachment);
+        const creationParams = {
+          username: atta.user, // TODO should be current user?
+          filepath: uploads.getRelativePath(targetPath),
+          mimetype: atta.mimetype,
+          thumbfilepath: uploads.getRelativePath(thumb.path),
+          thumbmimetype: thumb.mimetype,
+        };
 
-        return res.json({
-          attachment: attachmentWithUrls,
+        // Create new attachment
+        dal.create(creationParams, (errc, newAttachment) => {
+          if (errc) {
+            return next(errc);
+          }
+
+          return res.json({
+            attachment: urls.complete(newAttachment),
+          });
         });
       });
     });
