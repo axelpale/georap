15dd6cda34fe789269e858c50120cef7b15d5ae4
Akseli Palen <akseli.palen@gmail.com>
2021-03-18 21:04:57 +0200

    Remove orphaned appendToFilename fn

--- server/api/attachments/attachment/lib.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-exports.appendToFilename = (absPath, postfix) => {
-  const parts = path.parse(absPath);
-  return path.join(parts.dir, parts.name + postfix + parts.ext);
-};
