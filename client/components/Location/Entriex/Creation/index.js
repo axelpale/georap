bd712b9cbae114ff1aa27c157712cf1cc8456e11
Akseli Palen <akseli.palen@gmail.com>
2021-03-19 15:26:33 +0200

    Remember entry form contents until successful creation

--- client/components/Location/Entriex/Creation/index.js
+++ client/components/Location/Entriex/Creation/index.js
@@ -3,7 +3,13 @@ var emitter = require('component-emitter');
 var template = require('./template.ejs');
 var FormView = require('../Form');
 
-// eslint-disable-next-line no-unused-vars
+// Remember contents of unsubmitted forms during the session.
+// Cases where necessary:
+// - user fills the form but then exits to the map to search relevant info
+// - user fills the form but cancels by accident or to continue later
+//
+var formMemory = {}; // locId -> entryData
+
 module.exports = function (location) {
   // Parameters:
   //   location
@@ -14,21 +20,52 @@ module.exports = function (location) {
 
   var children = {};
 
+  var startFormMemory = function () {
+    formMemory[location._id] = {};
+  };
+  var getFormMemory = function () {
+    return formMemory[location._id];
+  };
+  var setFormMemory = function (entryData) {
+    if (entryData && location._id in formMemory) {
+      formMemory[location._id] = entryData;
+    }
+  };
+  var hasFormMemory = function () {
+    return (location._id in formMemory);
+  };
+  var stopFormMemory = function () {
+    delete formMemory[location._id];
+  };
+
   self.bind = function ($mount) {
     $mount.html(template({}));
 
-    children.form = new FormView(location);
+    if (hasFormMemory()) {
+      children.form = new FormView(location, getFormMemory());
+    } else {
+      children.form = new FormView(location);
+      startFormMemory();
+    }
+
     children.form.bind($mount.find('.entry-form-container'));
 
     children.form.once('exit', function () {
+      setFormMemory(children.form.getEntryData({ complete: true }));
       self.emit('exit');
     });
     children.form.on('success', function () {
+      stopFormMemory();
       self.emit('exit');
     });
   };
 
   self.unbind = function () {
+    // Memorize form contents on sudden exit if memory not stopped.
+    if (children.form && hasFormMemory()) {
+      setFormMemory(children.form.getEntryData({ complete: true }));
+    }
+    // Unbind
     ui.unbindAll(children);
   };
 };
