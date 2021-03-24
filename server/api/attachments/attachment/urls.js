7c09b2febc9f19edf1cdd747a0f47d7d4b49c517
Akseli Palen <akseli.palen@gmail.com>
2021-03-24 12:40:29 +0200

    attachment/urls.completeToUrl

--- server/api/attachments/attachment/urls.js
+++ server/api/attachments/attachment/urls.js
@@ -1,13 +1,17 @@
 const config = require('tresdb-config');
 const urljoin = require('url-join');
 
-exports.complete = function (attachment) {
+exports.complete = (attachment) => {
   return Object.assign({}, attachment, {
     url: urljoin(config.uploadUrl, attachment.filepath),
     thumburl: urljoin(config.uploadUrl, attachment.thumbfilepath),
   });
 };
 
-exports.completeEach = function (attachments) {
+exports.completeToUrl = (filepath) => {
+  return urljoin(config.uploadUrl, filepath);
+};
+
+exports.completeEach = (attachments) => {
   return attachments.map(exports.complete);
 };
