2ded5316583f21786e96c84ba161d1c7066f13a9
Akseli Palen <akseli.palen@gmail.com>
2021-03-25 14:06:27 +0200

    Move location to mloc conversion under location model

--- local_modules/tresdb-models/index.js
+++ local_modules/tresdb-models/index.js
@@ -10,18 +10,6 @@ exports.exports = require('./exports')
 exports.geometry = require('./geometry')
 exports.location = require('./location')
 
-exports.rawLocationToMarkerLocation = function (rawLoc) {
-  return {
-    _id: rawLoc._id,
-    name: rawLoc.name,
-    geom: rawLoc.geom,
-    status: rawLoc.status,
-    type: rawLoc.type,
-    layer: rawLoc.layer,
-    childLayer: rawLoc.layer
-  }
-}
-
 exports.bus = function (sourceBus) {
   // Return a local bus object that remembers the bound handlers
   // and thus provides an easy way to off them.
--- local_modules/tresdb-models/location.js
+++ local_modules/tresdb-models/location.js
@@ -4,3 +4,15 @@ var bus = require('./lib/bus')
 exports.bus = bus(function (loc, ev) {
   return ev.locationId === loc._id
 })
+
+exports.toMarkerLocation = function (loc) {
+  return {
+    _id: loc._id,
+    name: loc.name,
+    geom: loc.geom,
+    status: loc.status,
+    type: loc.type,
+    layer: loc.layer,
+    childLayer: loc.layer
+  }
+}
