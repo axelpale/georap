be29dee7c76575e850d56f53d3b43395d2b6a050
Akseli Palen <akseli.palen@gmail.com>
2021-04-02 22:37:38 +0300

    Extend minibus with sub

--- local_modules/tresdb-bus/index.js
+++ local_modules/tresdb-bus/index.js
@@ -1,1 +1,26 @@
-module.exports = require('minibus').create()
+var minibus = require('minibus');
+
+minibus.extension.sub = function () {
+  // Return a local bus object that remembers the bound handlers
+  // and thus provides an easy way to off them.
+  //
+  var self = this;
+  var routes = []
+  return {
+
+    on: function (evName, handler) {
+      var route = self.on(evName, handler)
+      routes.push(route)
+    },
+
+    off: function () {
+      self.off(routes)
+      routes = [] // for garbage collector
+    }
+
+  }
+}
+
+var rootBus = minibus.create()
+
+module.exports = rootBus
