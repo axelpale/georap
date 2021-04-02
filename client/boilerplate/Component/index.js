2a1a8acc64462f9f96bd516e6aeaccdc2cdbcd01
Akseli Palen <akseli.palen@gmail.com>
2021-04-02 14:26:44 +0300

    Extend boilerplate component

--- client/boilerplate/Component/index.js
+++ client/boilerplate/Component/index.js
@@ -1,18 +1,25 @@
+var emitter = require('component-emitter');
 var ui = require('tresdb-ui');
 var template = require('./template.ejs');
 
 module.exports = function () {
 
   // Setup
-  var self = this;
   var $mount = null;
   var children = {};
   var $elems = {};
+  var self = this;
+  emitter(self);
 
   self.bind = function ($mountEl) {
     $mount = $mountEl;
 
     $mount.html(template({}));
+
+    $elems.btn = $mount.find('.boilerplate-btn');
+    $elems.btn.click(function () {
+      console.log('Button clicked');
+    });
   };
 
   self.unbind = function () {
