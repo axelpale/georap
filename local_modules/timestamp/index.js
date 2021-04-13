8071c8ab34b37742fcc6d4978024416399a1dca0
Akseli Palen <akseli.palen@gmail.com>
2021-04-13 15:57:39 +0300

    Ensure timestamp tooltip visibility

--- local_modules/timestamp/index.js
+++ local_modules/timestamp/index.js
@@ -9,6 +9,6 @@ module.exports = function (isoTime) {
   //     string in ISO format. See ISO 8601
 
   return '<time datetime="' + isoTime + '" title="' + isoTime +
-         '" data-toggle="tooltip" data-placement="top">' +
+         '" data-toggle="tooltip" data-placement="bottom">' +
          timeago(isoTime) + '</time>';
 };
