5b0c29a815b81687dbe81541ab84721df03212af
Akseli Palen <akseli.palen@gmail.com>
2021-04-15 15:19:56 +0300

    Ensure only client side usage

--- local_modules/tresdb-urls/index.js
+++ local_modules/tresdb-urls/index.js
@@ -1,7 +1,17 @@
 /* eslint-disable no-var */
 
+if (!tresdb.config) {
+  throw new Error(
+    'No client-side configuration detected. ' +
+    'Ensure the urls module is called from client-side code ' +
+    'and after configuration is set.'
+  )
+}
+
+var staticUrl = tresdb.config.staticUrl
+
 exports.locationTypeToSymbolUrl = function (type) {
-  var baseUrl = '/assets/images/markers/symbols/'
+  var baseUrl = staticUrl + '/images/markers/symbols/'
   return baseUrl + type + '.png'
 }
 
