af8ef7cbb403387dfb217bacad59ec6e655213ba
Mika Tammi <mikatammi@gmail.com>
2021-04-22 20:36:18 +0300

    lintfix server/api/routes.js file
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- server/api/routes.js
+++ server/api/routes.js
@@ -1,24 +1,24 @@
 /* eslint-disable new-cap */
 
-var config = require('tresdb-config');
+const config = require('tresdb-config');
 
-var accountRouter = require('./account/routes');
-var adminRouter = require('./admin/routes');
-var attachmentsRouter = require('./attachments/routes');
-var eventsRouter = require('./events/routes');
-var geometryRouter = require('./geometry/routes');
-var iconsRouter = require('./icons/routes');
-var locationsRouter = require('./locations/routes');
-var markersRouter = require('./markers/routes');
-var paymentsRouter = require('./payments/routes');
-var statisticsRouter = require('./statistics/routes');
-var usersRouter = require('./users/routes');
+const accountRouter = require('./account/routes');
+const adminRouter = require('./admin/routes');
+const attachmentsRouter = require('./attachments/routes');
+const eventsRouter = require('./events/routes');
+const geometryRouter = require('./geometry/routes');
+const iconsRouter = require('./icons/routes');
+const locationsRouter = require('./locations/routes');
+const markersRouter = require('./markers/routes');
+const paymentsRouter = require('./payments/routes');
+const statisticsRouter = require('./statistics/routes');
+const usersRouter = require('./users/routes');
 
-var userDal = require('./users/user/dal');
+const userDal = require('./users/user/dal');
 
-var jwt = require('express-jwt');
-var status = require('http-status-codes');
-var router = require('express').Router();
+const jwt = require('express-jwt');
+const status = require('http-status-codes');
+const router = require('express').Router();
 
 // Icon routes do not require authentication. Thus before jwt middleware.
 router.use('/icons', iconsRouter);
@@ -46,7 +46,7 @@ router.use(jwt({
   algorithms: ['HS256'],
   getToken: function fromHeaderOrQuerystring(req) {
     // Copied from https://github.com/auth0/express-jwt#usage
-    var header = req.headers.authorization;
+    const header = req.headers.authorization;
     if (header && header.split(' ')[0] === 'Bearer') {
       return header.split(' ')[1];
     } else if (req.query && req.query.token) {
@@ -57,12 +57,12 @@ router.use(jwt({
 }));
 
 // Check if user is banned.
-router.use(function (req, res, next) {
+router.use((req, res, next) => {
 
   // Check this by querying the database, because it's SIMPLE.
   // This effectively nulls the benefits of using jwt tokens :DD
   // But what the hell...
-  userDal.getOne(req.user.name, function (err, storedUser) {
+  userDal.getOne(req.user.name, (err, storedUser) => {
     if (err) {
       return next(err);
     }
@@ -89,7 +89,7 @@ router.use('/users', usersRouter);
 
 // API error handling
 // See http://expressjs.com/en/guide/error-handling.html
-router.use(function (err, req, res, next) {
+router.use((err, req, res, next) => {
   // Token error handling
   if (err.name === 'UnauthorizedError') {
     return res.sendStatus(status.UNAUTHORIZED);
@@ -101,7 +101,7 @@ router.use(function (err, req, res, next) {
 
 // Catch all other non-errored API calls to 404.
 // Must be the final step in this router.
-router.get('/*', function (req, res) {
+router.get('/*', (req, res) => {
   console.log('404 Not Found: ' + req.originalUrl);
   return res.sendStatus(status.NOT_FOUND);
 });
