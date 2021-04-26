c083b97b3baeb9c758720349a0de5845c16ed7df
Akseli Palen <akseli.palen@gmail.com>
2021-04-26 11:49:58 +0300

    migration.lib.assertEvery and tape tests

---
+++ migration/lib/assertEvery.spec.js
@@ -0,0 +1,97 @@
+const test = require('tape');
+const assertEvery = require('./assertEvery');
+const config = require('tresdb-config');
+const db = require('tresdb-db');
+const loadFixture = require('./loadFixture');
+const dropCollections = require('./dropCollections');
+
+// Ensure we are in test mode before loading fixture
+if (process.env.NODE_ENV !== 'test') {
+  throw new Error('Tests not allowed in NODE_ENV ' + process.env.NODE_ENV);
+}
+
+const before = (fixture, done) => {
+  db.init(config.mongo.testUrl, (err) => {
+    if (err) {
+      return done(err);
+    }
+    // As a first step, drop all test db collections in case they
+    // have dirt after a bug or so.
+    dropCollections(db, (errdrop) => {
+      if (errdrop) {
+        return done(errdrop);
+      }
+
+      loadFixture(fixture, (errfix) => {
+        if (errfix) {
+          return done(errfix);
+        }
+
+        return done();
+      });
+    });
+  });
+};
+
+test.onFinish(() => {
+  // As a final step, reset the test database for future tests.
+  return dropCollections(db, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // Then, close and exit.
+    db.close();
+  });
+});
+
+test('assertEvery happy', (t) => {
+  const fixture = {
+    collections: {
+      docs: [
+        { key: true },
+        { key: true },
+        { key: true },
+      ],
+    },
+  };
+
+  before(fixture, (berr) => {
+    t.error(berr);
+    assertEvery('docs', (doc, next) => {
+      next(null, doc.key);
+    }, (err, result) => {
+      t.error(err, 'no errors');
+      t.equal(result.ok, true, 'result.ok');
+      t.equal(result.numValid, 3, 'numValid');
+      t.equal(result.numDocuments, 3, 'numDocuments');
+      t.end();
+    });
+  });
+});
+
+test('assertEvery short', (t) => {
+  const fixture = {
+    collections: {
+      docs: [
+        { key: true },
+        { key: false },
+        { key: true },
+      ],
+    },
+  };
+
+  before(fixture, (berr) => {
+    t.error(berr);
+    assertEvery('docs', (doc, next) => {
+      next(null, doc.key);
+    }, (err, result) => {
+      t.error(err, 'no errors');
+      t.equal(result.ok, false, 'result.ok');
+      t.equal(result.numValid, 1, 'numValid');
+      t.equal(result.numDocuments, 3, 'numDocuments');
+      t.end();
+    });
+  });
+});
