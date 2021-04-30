d1d09daeb1bee7601e5ded7fca139d952ea68a22
Akseli Palen <akseli.palen@gmail.com>
2021-05-01 00:09:23 +0300

    Remove tresdb-cap

--- local_modules/tresdb-cap/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (s) {
-  // Capitalize the first letter of string s.
-  if (typeof s !== 'string') {
-    return '';
-  }
-  return s.charAt(0).toUpperCase() + s.slice(1);
-};
