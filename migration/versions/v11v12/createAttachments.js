414a3508fe0d1066ba150bb4f0d0d27572f3f23d
Akseli Palen <akseli.palen@gmail.com>
2021-05-02 00:19:59 +0300

    Replicate attachmentDal.create in migration to avoid api change issues

--- migration/versions/v11v12/createAttachments.js
+++ migration/versions/v11v12/createAttachments.js
@@ -1,4 +1,6 @@
-const attachmentDal = require('../../../server/api/attachments/attachment/dal');
+const db = require('georap-db');
+const keygen = require('georap-key');
+const path = require('path');
 
 module.exports = (attachmentData, callback) => {
   // Parameters
@@ -13,16 +15,30 @@ module.exports = (attachmentData, callback) => {
   //   callback
   //     function (err, attachmentKeys)
   //
-  if (attachmentData.filepath) {
-    attachmentDal.create(attachmentData, (err, attachment) => {
-      if (err) {
-        return callback(err);
-      }
-
-      const keys = [attachment.key];
-      return callback(null, keys);
-    });
-  } else {
+  if (!attachmentData.filepath) {
     return callback(null, []);
   }
+
+  const attachment = {
+    key: keygen.generate(),
+    user: attachmentData.username,
+    time: attachmentData.time,
+    deleted: false,
+    filename: path.basename(attachmentData.filepath),
+    filepath: attachmentData.filepath,
+    mimetype: attachmentData.mimetype,
+    filesize: attachmentData.filesize,
+    thumbfilepath: attachmentData.thumbfilepath,
+    thumbmimetype: attachmentData.thumbmimetype,
+    data: {}, // For possible future metadata
+  };
+
+  db.collection('attachments').insertOne(attachment, (err) => {
+    if (err) {
+      return callback(err);
+    }
+
+    const keys = [attachment.key];
+    return callback(null, keys);
+  });
 };
