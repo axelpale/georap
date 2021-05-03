db61b97da5350d37768ebc09a0b7f6e707894596
Akseli Palen <akseli.palen@gmail.com>
2021-05-03 21:23:39 +0300

    Create location.dal.getAttachments

--- server/api/locations/location/dal/index.js
+++ server/api/locations/location/dal/index.js
@@ -3,6 +3,7 @@ exports.changeName = require('./changeName');
 exports.changeStatus = require('./changeStatus');
 exports.changeType = require('./changeType');
 exports.changeThumbnail = require('./changeThumbnail');
+exports.getAttachments = require('./getAttachments');
 exports.getOneComplete = require('./getOneComplete');
 exports.getRaw = require('./getRaw');
 exports.removeOne = require('./removeOne');
--- server/api/locations/location/handlers/getAttachments.js
+++ server/api/locations/location/handlers/getAttachments.js
@@ -1,5 +1,4 @@
-const getManyComplete = require('../../../entries/dal/getManyComplete');
-const attachmentsModel = require('georap-models').attachments;
+const locationDal = require('../dal');
 
 module.exports = (req, res, next) => {
   // Get location entries with their attachments and urls completed.
@@ -7,41 +6,17 @@ module.exports = (req, res, next) => {
   // Query parameters:
   //   imagesOnly
   //     optional string. Set 'true' to include only image attachments.
-
-  const imagesOnly = (req.query.imagesOnly === 'true');
-
-  // NOTE getManyComplete does a bit unnecessary work to preserve
-  // structure entries and comments although we only need their attachments.
-  // However, the heavy lifting parts are the same and therefore it is
-  // a good dry way to get the attachments.
-  getManyComplete({
+  //
+  locationDal.getAttachments({
     locationId: req.location._id,
-    deleted: false,
-  }, {
-    // NOTE skip and limit already validated by middleware
-    skip: 0,
-    limit: 100, // meaning: infinity
-  }, (err, entries) => {
+    imagesOnly: (req.query.imagesOnly === 'true'),
+  }, (err, attachments) => {
     if (err) {
       return next(err);
     }
 
-    // Collect attachments
-    let atts = [];
-    entries.forEach((entry) => {
-      atts = atts.concat(entry.attachments);
-      entry.comments.forEach((comment) => {
-        atts = atts.concat(comment.attachments);
-      });
-    });
-
-    // Filter images if so requested
-    if (imagesOnly) {
-      atts = attachmentsModel.getImages(atts);
-    }
-
     return res.json({
-      attachments: atts,
+      attachments: attachments,
     });
   });
 };
