7d84d1669e28786a1133d45151b6d9d68d49218c
Akseli Palen <akseli.palen@gmail.com>
2021-05-03 16:31:31 +0300

    Add locations.getAttachments api adapter

--- client/stores/locations.js
+++ client/stores/locations.js
@@ -163,6 +163,38 @@ exports.get = function (id, callback) {
   });
 };
 
+exports.getAttachments = function (params, callback) {
+  // Fetch all attachments related to the location.
+  // The result can be used for example for the location thumbnail palette.
+  //
+  // Parameters:
+  //   params
+  //     locationId
+  //     imagesOnly
+  //       optional boolean, default false
+  //   callback
+  //     function (err, { attachments }) where
+  //       attachments
+  //         array of attachment objects
+  //
+  params = Object.assign({
+    imagesOnly: false,
+  }, params);
+
+  return getJSON({
+    url: '/api/locations/' + params.locationId + '/attachments',
+    data: {
+      imagesOnly: params.imagesOnly,
+    },
+  }, function (err, result) {
+    if (err) {
+      return callback(err);
+    }
+
+    return callback(null, result);
+  });
+};
+
 exports.getEntries = function (params, callback) {
   // Fetch a set of entries of the location, most recent first.
   //
