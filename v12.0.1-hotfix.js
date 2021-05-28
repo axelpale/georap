69e3c48cf49de854dcbe8c312905a6f013e7ae45
Akseli Palen <akseli.palen@gmail.com>
2021-05-28 15:06:57 +0300

    Note when v12.0.1 hotfix is needed

--- v12.0.1-hotfix.js
+++ v12.0.1-hotfix.js
@@ -1,5 +1,10 @@
 // Repairs missing createdAt timestamps
 //
+// You should run this if you upgraded directly to v12.0.0 and
+// any locations were created until upgrade to v12.0.1 or later.
+// If you upgraded from v11.x directly to v12.0.1 or later, you can safely
+// ignore this hotfix.
+//
 // Run:
 //   $ NODE_ENV=production node v12.0.1-hotfix.js
 //
