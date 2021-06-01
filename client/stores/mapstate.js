5ed715c53c645722b9b7fa24c8fa6362e14298d1
Akseli Palen <akseli.palen@gmail.com>
2021-06-02 00:51:18 +0300

    Document mapstate store api

--- client/stores/mapstate.js
+++ client/stores/mapstate.js
@@ -1,13 +1,25 @@
 // This Store takes care of reading and storing a map viewport state
 // to a given storage, e.g. localStorage.
 //
-// Structure:
+// The structure of the state:
 //   {
 //     lat: <number>,
 //     lng: <number>,
 //     zoom: <integer>,
 //     mapTypeId: <string>,
 //   }
+//
+// API
+//   Constructor
+//     createStore(storage, storageKey, defaultState)
+//   Methods
+//     update(stateDelta, opts)
+//     reset()
+//     isDefault()
+//     isEmpty()
+//     get()
+//   Emits
+//     'updated' with state
 //
 var createStore = require('./lib/createStore');
 var storage = require('../connection/storage');
