44ce2457d9e5ab70daec10591dad7065749afc31
Akseli Palén <akseli.palen@gmail.com>
2021-06-28 12:54:37 +0300

    Convert card-close-icon to class

--- client/components/Card/closeButton/index.js
+++ client/components/Card/closeButton/index.js
@@ -2,10 +2,10 @@ var template = require('./template.ejs');
 
 module.exports = function ($mount, cardType, onClick) {
   // Remove possible previous icon
-  $mount.find('#card-close-icon').remove();
+  $mount.find('.card-close-icon').remove();
   if (cardType === 'page') {
     $mount.prepend(template({}));
-    var $close = $mount.find('#card-close-icon');
+    var $close = $mount.find('.card-close-icon');
     $close.off('click');
     $close.click(onClick);
   }
