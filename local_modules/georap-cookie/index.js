49e0c9225d4f4cfafd0ec09c016bd33b533eb6b7
Akseli Palen <akseli.palen@gmail.com>
2021-12-18 00:19:08 +0200

    lint georap-cookie

--- local_modules/georap-cookie/index.js
+++ local_modules/georap-cookie/index.js
@@ -5,32 +5,33 @@
 //
 
 exports.setCookie = function (name, value, days, httpOnly) {
-  var expires = '';
-  var secure = '';
+  let expires = ''
+  let secure = ''
 
   if (days) {
-    var at = Date.now() + days * 24 * 60 * 60 * 1000; // ms
-    expires = '; expires=' + (new Date(at)).toUTCString();
+    const at = Date.now() + days * 24 * 60 * 60 * 1000 // ms
+    expires = '; expires=' + (new Date(at)).toUTCString()
   }
   if (httpOnly) {
-    secure = '; secure';
+    secure = '; secure'
   }
 
-  return document.cookie = name + '=' + encodeURIComponent(value) +
-    expires + secure;
-};
+  // Set new cookie. Note, this does not overwrite other cookies.
+  document.cookie = name + '=' + encodeURIComponent(value) +
+    expires + secure
+}
 
 exports.getCookie = function (name) {
   // Find the cookie form the stringified list
-  var attribs = ('; ' + document.cookie).split('; ' + name + '=')[1];
+  const attribs = ('; ' + document.cookie).split('; ' + name + '=')[1]
   if (attribs) {
     // Pick only the value
-    return decodeURIComponent(attribs.split(';')[0]);
+    return decodeURIComponent(attribs.split(';')[0])
   }
   // No such cookie
-  return '';
-};
+  return ''
+}
 
 exports.removeCookie = function (name) {
-  document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT';
-};
+  document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT'
+}
