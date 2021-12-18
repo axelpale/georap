687ebe883d2cb6a2fd3d911f0f1b465d5df57d95
Akseli Palen <akseli.palen@gmail.com>
2021-12-18 22:50:00 +0200

    Languages subsection in Account component, sketch

--- client/components/Account/index.js
+++ client/components/Account/index.js
@@ -1,7 +1,9 @@
 var template = require('./template.ejs');
 var emitter = require('component-emitter');
+var components = require('georap-components');
 var ui = require('georap-ui');
 var themeStore = georap.stores.theme;
+var localeStore = georap.stores.locale;
 var availableLocales = georap.config.availableLocales;
 
 var colorSchemes = ['light', 'dark'];
@@ -23,6 +25,9 @@ module.exports = function () {
       availableLocales: availableLocales,
     }));
 
+    $elems.localeSelector = $mount.find('.locale-selector');
+    $elems.localeErrorContainer = $mount.find('.locale-error-container');
+
     colorSchemes.forEach(function (colorScheme, i) {
       var elemid = 'theme-' + colorScheme;
       $elems[elemid] = $('#' + elemid);
@@ -31,6 +36,16 @@ module.exports = function () {
           colorScheme: colorScheme,
           themeColor: themeColors[i],
         });
+      });
+    });
+
+    $elems.localeSelector.on('change', function () {
+      var selectedLocale = $elems.localeSelector.val();
+      localeStore.setLocale(selectedLocale, function (err) {
+        if (err) {
+          children.localeError = new components.Error(err.message);
+          children.localeError.bind($elems.localeErrorContainer);
+        }
       });
     });
   };
