6cc6d4df2d061f847aaa6e6632cc934f84ca1610
Akseli Palen <akseli.palen@gmail.com>
2022-01-15 20:26:15 +0200

    Sketchy georap-components.Remover

---
+++ local_modules/georap-components/Remover/index.js
@@ -0,0 +1,67 @@
+
+var Opener = require('../Opener');
+var RemoverPanel = require('./RemoverPanel');
+var ui = require('georap-ui');
+var emitter = require('component-emitter');
+
+module.exports = function (params) {
+  // Removal button and confirmation.
+  //
+  // Parameters:
+  //   params, with required props
+  //     submit
+  //       function (), a submit handler.
+  //         Called when user confirms deletion.
+  //     cancelBtnText
+  //       string
+  //     deleteBtnText
+  //       string
+  //     infoText
+  //       string
+  //     youSureText
+  //       string
+  //
+  // Emit
+  //   submit
+  //     on confirmed deletion attempt
+  //
+  var $container = null;
+  var $button = null;
+  var opener = null;
+  var self = this;
+  emitter(self);
+
+  self.bind = function (mounts) {
+    $container = mounts.$container;
+    $button = mounts.$button;
+
+    var panel = new RemoverPanel({
+      cancelBtnText: params.cancelBtnText,
+      deleteBtnText: params.deleteBtnText,
+      infoText: params.infoText,
+      youSureText: params.youSureText,
+    });
+    opener = new Opener(panel, false);
+    opener.bind({
+      $container: $container,
+      $button: $button,
+    });
+
+    opener.on('submit', function (ev) {
+      ev.preventDefault();
+      if (typeof params.submit === 'function') {
+        params.submit();
+      }
+    });
+  };
+
+  self.unbind = function () {
+    if (opener) {
+      opener.off('submit');
+      opener.unbind();
+      opener = null;
+      $container = null;
+      $button = null;
+    }
+  };
+};
--- local_modules/georap-components/index.js
+++ local_modules/georap-components/index.js
@@ -1,5 +1,10 @@
+// Reusable components for Georap
+//
+// Assume client-side browser environment
+//
 
 exports.Error = require('./Error')
 exports.Opener = require('./Opener')
 exports.Progress = require('./Progress')
+exports.Remover = require('./Remover')
 exports.Thumbnail = require('./Thumbnail')
