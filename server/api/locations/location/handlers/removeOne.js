a15b7a90f23d0ea59e7f226f9e1d3ce5c956b8a2
Akseli Palen <akseli.palen@gmail.com>
2022-01-20 00:35:07 +0200

    Special ownership check on location removal

--- server/api/locations/location/handlers/removeOne.js
+++ server/api/locations/location/handlers/removeOne.js
@@ -1,15 +1,24 @@
 const locationDal = require('../dal');
 const status = require('http-status-codes');
+const isAble = require('georap-able').isAble;
+
+const isAbleToRemove = function (user, location) {
+  // Allow only admins and creators to delete if they are able.
+  if (isAble(user, 'locations-delete-any')) {
+    return true;
+  } else if (isAble(user, 'locations-delete-own')) {
+    if (user.name === location.creator) {
+      return true;
+    }
+  }
+  return false;
+};
 
 module.exports = (req, res, next) => {
   // Delete single location
 
   // Ensure only owner or admin can delete
-  const username = req.user.name;
-  const role = req.user.role;
-  const creator = req.location.creator;
-
-  if (username !== creator && role !== 'admin') {
+  if (!isAbleToRemove(req.user, req.location)) {
     return res.sendStatus(status.FORBIDDEN);
   }
 
