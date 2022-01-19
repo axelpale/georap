09d90dd66b6d77ba412f2ad9725d03e7952b4e72
Akseli Palen <akseli.palen@gmail.com>
2022-01-20 00:29:02 +0200

    New local_module georap-able

---
+++ local_modules/georap-able/index.js
@@ -0,0 +1,34 @@
+// Capability middleware.
+//
+const config = require('georap-config')
+const status = require('http-status-codes')
+
+exports.isAble = function (user, cap) {
+  // Return
+  //   boolean
+  //
+  const capn = cap.toLowerCase() // normalize
+
+  let role = 'public'
+  if (user && user.role) {
+    role = user.role
+  }
+  // Capabilities of this role
+  const caps = config.capabilities[role]
+
+  // Do the capabilities include the required capability.
+  return caps.includes(capn)
+}
+
+exports.middleware = function (cap) {
+  // Returns a middleware function (req, res, next)
+  //   that calls next if user has the required capability.
+  //
+  return function (req, res, next) {
+    if (exports.isAble(req.user, cap)) {
+      return next()
+    }
+    const msg = 'You are not capable of accessing this resource'
+    return res.status(status.FORBIDDEN).send(msg)
+  }
+}
