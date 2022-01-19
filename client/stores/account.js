300ed76cfe194f8d206036beef6853463d8a7607
Akseli Palen <akseli.palen@gmail.com>
2022-01-19 18:16:57 +0200

    Organise account store methods to api calls and non-api functions.

--- client/stores/account.js
+++ client/stores/account.js
@@ -43,6 +43,180 @@ var user = (function () {
 }());
 
 
+// Token methods without API calls
+
+exports.getEmail = function () {
+  if (user) {
+    return user.email;
+  }
+  throw new Error('Non-existent user cannot have a name');
+};
+
+exports.getName = function () {
+  // Return username as a string
+  if (user) {
+    return user.name;
+  }
+  throw new Error('Non-existent user cannot have a name');
+};
+
+exports.getRole = function () {
+  if (user) {
+    return user.role;
+  }
+  return 'public';
+};
+
+exports.getToken = function () {
+  // Can be called only if isLoggedIn.
+  if (user) {
+    return storage.getItem(TOKEN_KEY);
+  }
+  throw new Error('The token is missing.');
+};
+
+exports.getUser = function () {
+  // Get user object:
+  // {
+  //   name: <string>
+  //   email: <string>
+  //   role: <string>
+  // }
+  //
+  // If no user set, will return null.
+  //
+  // TODO MAYBE If public user, will return
+  //   {
+  //     name: 'Anon',
+  //     email: 'anonymous@example.com',
+  //     role: 'public',
+  //   }
+  //
+  return user;
+};
+
+exports.isAble = function (cap) {
+  // Returns bool to tell if the user is capable of the given capability.
+  //
+  // Parameters
+  //   cap
+  //     string, a capability name
+  //
+  // Dev note: this fn does not make things more secure.
+  //   This fn is only for user interface correctness.
+  //   The real security must be enforced on server-side.
+  //
+
+  // Normalise to lower case
+  var capn = cap.toLowerCase();
+
+  // User role
+  var role = 'public';
+  if (user) {
+    role = user.role;
+  }
+
+  if (caps[role]) {
+    if (caps[role].indexOf(capn) > -1) {
+      // User is able only if known user's role has the capability.
+      return true;
+    }
+  }
+
+  // User is not able
+  return false;
+};
+
+exports.isAdmin = function () {
+  // Return
+  //   bool, true if admin, false if not admin or not logged in.
+  //
+  if (user && user.role === 'admin') {
+    return true;
+  }
+  return false;
+};
+
+exports.isLoggedIn = function () {
+  // True if user is authenticated. Requires the token to be valid.
+  //
+  if (user && user.role !== 'public') {
+    return true;
+  }
+  return false;
+};
+
+exports.isMe = function (username) {
+  // Test if current user has this username.
+  if (user) {
+    return username === user.name;
+  }
+  return false;
+};
+
+exports.isRoleAble = function (role, cap) {
+  // Tells if the given role has the given capability.
+  //
+
+  // Normalise to lower case
+  var capn = cap.toLowerCase();
+  // Role is able only if it is configured and has capability.
+  if (caps[role]) {
+    if (caps[role].indexOf(capn) > -1) {
+      return true;
+    }
+  }
+  return false;
+};
+
+exports.logout = function (callback) {
+  // Parameters:
+  //   callback
+  //     function (err), if successful, err === null
+  // Emits:
+  //   logout
+  //     On successful logout
+
+  // NOTE The token is removed but the server will still accept it
+  // until the token has expired. This is a feature/weakness of JWT token.
+
+  user = null;
+  storage.removeItem(TOKEN_KEY);
+  exports.emit('logout');
+
+  if (typeof callback !== 'undefined') {
+    return callback(null);
+  }
+};
+
+exports.setToken = function (token) {
+  // Set token. Ensure token validity.
+  //
+  // Parameters
+  //   token
+  //     string, a JWT token
+  //
+  // Return
+  //   boolean, true if token set successful, false otherwise.
+  if (token) {
+    var decoded = jwtDecode(token);
+    if (decoded.exp > Date.now() / 1000) {
+      // Success
+      user = decoded;
+      storage.setItem(TOKEN_KEY, token);
+      return true;
+    }
+  }
+
+  // Else invalid. It is best to log the user out.
+  user = null;
+  storage.removeItem(TOKEN_KEY);
+  return false;
+};
+
+
+// API methods
+
 exports.login = function (email, password, callback) {
   // Parameters:
   //   email
@@ -94,26 +268,6 @@ exports.login = function (email, password, callback) {
       return callback(err);
     },
   });
-};
-
-exports.logout = function (callback) {
-  // Parameters:
-  //   callback
-  //     function (err), if successful, err === null
-  // Emits:
-  //   logout
-  //     On successful logout
-
-  // NOTE The token is removed but the server will still accept it
-  // until the token has expired. This is a feature/weakness of JWT token.
-
-  user = null;
-  storage.removeItem(TOKEN_KEY);
-  exports.emit('logout');
-
-  if (typeof callback !== 'undefined') {
-    return callback(null);
-  }
 };
 
 exports.changeEmailSendCode = function (newEmail, callback) {
@@ -205,16 +359,7 @@ exports.changePassword = function (currentPassword, newPassword, callback) {
   });
 };
 
-exports.isMe = function (username) {
-  // Test if current user has this username.
-  if (user) {
-    return username === user.name;
-  }
-  return false;
-};
-
 exports.sendResetPasswordEmail = function (email, callback) {
-
   $.ajax({
     url: '/api/account/reset/email',
     type: 'POST',
@@ -232,7 +377,6 @@ exports.sendResetPasswordEmail = function (email, callback) {
 };
 
 exports.resetPassword = function (resetToken, newPassword, callback) {
-
   $.ajax({
     url: '/api/account/reset',
     type: 'POST',
@@ -308,122 +452,6 @@ exports.signup = function (signupToken, username, password, callback) {
   });
 };
 
-exports.isLoggedIn = function () {
-  // True if user is authenticated. Requires the token to be valid.
-  //
-  if (user && user.role !== 'public') {
-    return true;
-  }
-  return false;
-};
-
-exports.isAble = function (cap) {
-  // Returns bool to tell if the user is capable of the given capability.
-  //
-  // Parameters
-  //   cap
-  //     string, a capability name
-  //
-  // Dev note: this fn does not make things more secure.
-  //   This fn is only for user interface correctness.
-  //   The real security must be enforced on server-side.
-  //
-
-  // Normalise to lower case
-  var capn = cap.toLowerCase();
-
-  // User role
-  var role = 'public';
-  if (user) {
-    role = user.role;
-  }
-
-  if (caps[role]) {
-    if (caps[role].indexOf(capn) > -1) {
-      // User is able only if known user's role has the capability.
-      return true;
-    }
-  }
-
-  // User is not able
-  return false;
-};
-
-exports.isRoleAble = function (role, cap) {
-  // Tells if the given role has the given capability.
-  //
-
-  // Normalise to lower case
-  var capn = cap.toLowerCase();
-  // Role is able only if it is configured and has capability.
-  if (caps[role]) {
-    if (caps[role].indexOf(capn) > -1) {
-      return true;
-    }
-  }
-  return false;
-};
-
-exports.isAdmin = function () {
-  // Return
-  //   bool, true if admin, false if not admin or not logged in.
-  //
-  if (user && user.role === 'admin') {
-    return true;
-  }
-  return false;
-};
-
-exports.getToken = function () {
-  // Can be called only if isLoggedIn.
-  if (user) {
-    return storage.getItem(TOKEN_KEY);
-  }
-  throw new Error('The token is missing.');
-};
-
-exports.getUser = function () {
-  // Get user object:
-  // {
-  //   name: <string>
-  //   email: <string>
-  //   role: <string>
-  // }
-  //
-  // If no user set, will return null.
-  //
-  // TODO MAYBE If public user, will return
-  //   {
-  //     name: 'Anon',
-  //     email: 'anonymous@example.com',
-  //     role: 'public',
-  //   }
-  //
-  return user;
-};
-
-exports.getEmail = function () {
-  if (user) {
-    return user.email;
-  }
-  throw new Error('Non-existent user cannot have a name');
-};
-
-exports.getName = function () {
-  // Return username as a string
-  if (user) {
-    return user.name;
-  }
-  throw new Error('Non-existent user cannot have a name');
-};
-
-exports.getRole = function () {
-  if (user) {
-    return user.role;
-  }
-  return 'public';
-};
-
 exports.getVisitedLocationIds = function (callback) {
   // Return array of ids of locations that the user has visited.
   return users.getVisitedLocationIds(exports.getName(), callback);
@@ -436,29 +464,4 @@ exports.getFlags = function (callback) {
   //       flags is a mapping locationId -> array of flags set by the user
   //
   return users.getFlags(exports.getName(), callback);
-};
-
-exports.setToken = function (token) {
-  // Set token. Ensure token validity.
-  //
-  // Parameters
-  //   token
-  //     string, a JWT token
-  //
-  // Return
-  //   boolean, true if token set successful, false otherwise.
-  if (token) {
-    var decoded = jwtDecode(token);
-    if (decoded.exp > Date.now() / 1000) {
-      // Success
-      user = decoded;
-      storage.setItem(TOKEN_KEY, token);
-      return true;
-    }
-  }
-
-  // Else invalid. It is best to log the user out.
-  user = null;
-  storage.removeItem(TOKEN_KEY);
-  return false;
 };
