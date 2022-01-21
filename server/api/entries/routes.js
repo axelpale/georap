15f2e2640f4c4f9e0385706bff7f7d1e121b4bb9
Akseli Palen <akseli.palen@gmail.com>
2022-01-21 22:56:00 +0200

    Sketchy posts cababilities

--- server/api/entries/routes.js
+++ server/api/entries/routes.js
@@ -9,15 +9,16 @@ const entryIdParser = require('./lib/entryIdParser');
 const locationIdParser = require('./lib/locationIdParser');
 const commentsRouter = require('./comments/routes');
 
-router.post('/', jsonParser, locationIdParser, handlers.create);
 router.get('/', skipLimitParser, handlers.latest);
+router.post('/', able('posts-create'), jsonParser,
+            locationIdParser, handlers.create);
 
 router.use('/:entryId', entryIdParser);
 
-router.post('/:entryId', jsonParser, handlers.change);
-router.delete('/:entryId', handlers.remove);
+router.post('/:entryId', able('posts-update'), jsonParser, handlers.change);
+router.delete('/:entryId', able('posts-delete'), handlers.remove);
 
-router.post('/:entryId/move', jsonParser, handlers.move);
+router.post('/:entryId/move', able('posts-move'), jsonParser, handlers.move);
 router.use('/:entryId/comments', able('comments-read'), commentsRouter);
 
 module.exports = router;
