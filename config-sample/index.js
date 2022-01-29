fb184dd09cef7b500d9f29662e19dae6e6d810a1
Akseli Palen <akseli.palen@gmail.com>
2022-01-29 05:20:04 +0200

    extract post-related config to submodule

--- config-sample/index.js
+++ config-sample/index.js
@@ -24,6 +24,7 @@
 // We will join paths relative to the current directory.
 const path = require('path');
 const icons = require('./icons');
+const posts = require('./posts');
 const filesUrls = require('./filesUrls');
 const loginPage = require('./loginPage');
 const capabilities = require('./capabilities');
@@ -258,43 +259,12 @@ module.exports = {
     },
   },
 
-  // Flags for Posts.
-  // Flags are used to classify posts (aka entries) or give them perks.
-  // An example of a flag is 'visit' that can denote that the post is
-  // about a visitation on the location instead of just general info.
-  // Flags also accumulate to the location of the flagged post, so
-  // that a user can filter locations based on flags in their own posts.
-  // For example the 'visit' flag allows a user to browse all locations
-  // she has visited.
-  // Translation of flags: to display flags in different languages
-  // the name and description of the flag are in fact translation keys.
-  // Because the flags are customized by you, Georap cannot know translations
-  // for them automatically. For that, you must give translations for the keys
-  // in the custom locales under config/locales.
-  entryFlags: {
-    visit: {
-      name: 'visit', // a translation key
-      genitive: 'visit-genitive', // a translation key
-      plural: 'visit-plural', // a translation key
-      description: 'visit-description', // a translation key
-      glyphicon: 'flag',
-      reward: 15,
-      // Precondition allows a flag to be used only if the post content
-      // fulfills a condition. Post content consists of
-      // the post properties { markdown, attachments, flags }.
-      // The condition is represented as JSON schema.
-      precondition: {
-        type: 'object',
-        properties: {
-          attachments: {
-            type: 'array',
-            minItems: 1,
-          },
-        },
-        required: ['attachments'],
-      },
-    },
-  },
+  // Post list view settings
+  entries: posts.entries,
+  // Commenting
+  comments: posts.comments,
+  // Posts can be flagged for example as visits or questions.
+  entryFlags: posts.entryFlags,
 
   // Marker templates.
   // Marker template is a background image for the marker without a symbol.
@@ -430,19 +400,6 @@ module.exports = {
         'lg': 'yellow_default_lg',
       },
     },
-  },
-
-  // Post list view settings
-  entries: {
-    // Items to load initially and when Load More -button is pressed.
-    pageSize: 10,
-  },
-
-  // Commenting
-  comments: {
-    secondsEditable: 360,
-    minMessageLength: 2,
-    maxMessageLength: 600,
   },
 
   // Support page
