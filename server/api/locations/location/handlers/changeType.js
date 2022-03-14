1e4003e810b4dd46c5d3f9956bfdac167dc8ae63
Akseli Palen <akseli.palen@gmail.com>
2022-03-14 15:13:10 +0200

    accept status parameter in location type api

--- server/api/locations/location/handlers/changeType.js
+++ server/api/locations/location/handlers/changeType.js
@@ -6,10 +6,24 @@ module.exports = (req, res, next) => {
   // Update location type
   //
 
+  const oldStatus = req.location.status;
+  const newStatus = req.body.status;
+  const oldType = req.location.type;
+  const newType = req.body.type;
+
+  // Validate status
+  if (typeof newStatus === 'string') {
+    if (config.locationStatuses.indexOf(newStatus) < 0) {
+      const msg = 'Invalid location status: ' + newStatus;
+      return res.status(status.BAD_REQUEST).send(msg);
+    }
+  } else {
+    return res.status(status.BAD_REQUEST).send('Invalid location status');
+  }
   // Validate type
-  if (typeof req.body.type === 'string') {
-    if (config.locationTypes.indexOf(req.body.type) < 0) {
-      const msg = 'Invalid location type: ' + req.body.type;
+  if (typeof newType === 'string') {
+    if (config.locationTypes.indexOf(newType) < 0) {
+      const msg = 'Invalid location type: ' + newType;
       return res.status(status.BAD_REQUEST).send(msg);
     }
   } else {
@@ -17,18 +31,19 @@ module.exports = (req, res, next) => {
   }
 
   // If no change, everything ok already
-  const oldType = req.location.type;
-  const newType = req.body.type;
-  if (oldType === newType) {
-    return res.status(status.OK).send('Type not changed. Same already.');
+  if (oldStatus === newStatus && oldType === newType) {
+    const msg = 'Status and type did not change. Same already.';
+    return res.status(status.OK).send(msg);
   }
 
   locationDal.changeType({
     locationId: req.location._id,
     locationName: req.location.name,
-    locationType: oldType,
     username: req.user.name,
-    type: newType,
+    oldStatus: oldStatus,
+    newStatus: newStatus,
+    oldType: oldType,
+    newType: newType,
   }, (err) => {
     if (err) {
       return next(err);
