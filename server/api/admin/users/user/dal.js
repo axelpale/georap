d011f9cd3aa09c6f9cdcbac7fd7788393c31a11a
Akseli Palen <akseli.palen@gmail.com>
2026-01-16 15:17:38 +0200

    sketch user admin emergency feature: resetPassword

--- server/api/admin/users/user/dal.js
+++ server/api/admin/users/user/dal.js
@@ -1,5 +1,7 @@
 const config = require('georap-config');
 const db = require('georap-db');
+const bcrypt = require('bcryptjs');
+const keygen = require('georap-key');
 
 exports.getUserForAdmin = function (username, callback) {
   // Fetch an array of users with admin-only information such as email.
@@ -63,6 +65,54 @@ exports.setRole = function (username, newRole, callback) {
       return callback(err);
     }
     return callback();
+  });
+};
+
+
+exports.resetPassword = function (username, callback) {
+  // Reset user password with a random one.
+  //
+  // DANGER: Use with caution. It is bad practice to let
+  // admin users to observe passwords of users, even when
+  // the passwords are randomly generated. This lowers
+  // the threshold for a hacked admin account to access
+  // and impersonate other users.
+  //
+  // This function is not used or tested and it is here
+  // for future reference.
+  //
+  // Parameters:
+  //   username
+  //     string
+  //   callback
+  //     function (err, newPassword)
+  //
+  if (typeof username !== 'string') {
+    throw new Error('invalid parameters');
+  }
+
+  // Generate new password.
+  const newPassword = keygen.generate();
+
+  // Hash the password for storage.
+  const r = config.bcrypt.rounds;
+  bcrypt.hash(newPassword, r, (berr, pwdHash) => {
+    if (berr) {
+      return callback(berr);
+    }
+
+    const coll = db.collection('users');
+    const q = { name: username };
+    const up = { $set: { 'hash': pwdHash } };
+
+    coll.updateOne(q, up, (err) => {
+      if (err) {
+        return callback(err);
+      }
+
+      // Return the new password for the admin to see and pass physically.
+      return callback(null, newPassword);
+    });
   });
 };
 
